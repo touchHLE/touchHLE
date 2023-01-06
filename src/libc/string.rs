b8f3ceec43828776426dfeb740ddfbeb8973c2ee
hikari_no_yume <hikari@noyu.me>
2023-01-06 19:44:50 +0100

    Implement strcmp()

--- src/libc/string.rs
+++ src/libc/string.rs
@@ -3,6 +3,7 @@
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::mem::{ConstPtr, GuestUSize, MutPtr};
 use crate::Environment;
+use std::cmp::Ordering;
 
 fn strlen(env: &mut Environment, s: ConstPtr<u8>) -> GuestUSize {
     let mut size = 0;
@@ -34,8 +35,30 @@ fn strcat(env: &mut Environment, dest: MutPtr<u8>, src: ConstPtr<u8>) -> MutPtr<
     dest
 }
 
+fn strcmp(env: &mut Environment, a: ConstPtr<u8>, b: ConstPtr<u8>) -> i32 {
+    let mut offset = 0;
+    loop {
+        let char_a = env.mem.read(a + offset);
+        let char_b = env.mem.read(b + offset);
+        offset += 1;
+
+        match char_a.cmp(&char_b) {
+            Ordering::Less => return -1,
+            Ordering::Greater => return 1,
+            Ordering::Equal => {
+                if char_a == b'\0' {
+                    return 0;
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+}
+
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(strlen(_)),
     export_c_func!(strcpy(_, _)),
     export_c_func!(strcat(_, _)),
+    export_c_func!(strcmp(_, _)),
 ];

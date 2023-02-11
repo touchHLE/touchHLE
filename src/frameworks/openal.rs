548fae299d5021d4a49a09d95bb35ac9b3ccb2a4
nitinseshadri <61258126+nitinseshadri@users.noreply.github.com>
2023-02-11 13:30:00 -0500

    Remove Monkey Ball-specific alcMacOSXMixerOutputRate hack No longer needed now that we provide a stub for alcMacOSXMixerOutputRate.

--- src/frameworks/openal.rs
+++ src/frameworks/openal.rs
@@ -124,16 +124,6 @@ fn alcGetProcAddress(
     _device: ConstPtr<GuestALCdevice>,
     func_name: ConstPtr<u8>,
 ) -> ConstVoidPtr {
-    // Apple-specific extension that Super Monkey Ball tries to use.
-    // Conveniently, if NULL is returned, it just skips trying to use it, so
-    // let's do that.
-    if env.mem.cstr_at_utf8(func_name) == "alcMacOSXMixerOutputRate" {
-        // Warn in case other apps don't check for NULL. The spec doesn't even
-        // mention that as a possibility.
-        log!("Returning NULL for alcGetProcAddress(..., \"alcMacOSXMixerOutputRate\").");
-        return Ptr::null();
-    }
-
     let mangled_func_name = format!("_{}", env.mem.cstr_at_utf8(func_name));
     assert!(mangled_func_name.starts_with("_al"));
 
@@ -293,11 +283,7 @@ fn alBufferDataStatic(
 
 // Apple-specific extension to OpenAL
 fn alcMacOSXMixerOutputRate(_env: &mut Environment, value: ALdouble) {
-    // TODO: actually implement this
-    println!(
-        "Warning: Not implemented yet: App wants to set mixer output sample rate: {}",
-        value
-    )
+    log!("App wants to set mixer output sample rate to {} Hz", value);
 }
 
 // TODO: more functions

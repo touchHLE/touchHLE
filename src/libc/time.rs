17fc64fa1876d010fa0d4662f65875dd1b973094
hikari_no_yume <hikari@noyu.me>
2023-02-11 15:21:26 +0100

    Implement gettimeofday()

--- src/libc/time.rs
+++ src/libc/time.rs
@@ -3,10 +3,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-//! `time.h`
+//! `time.h` (C) and `sys/time.h` (POSIX)
 
 use crate::dyld::{export_c_func, FunctionExports};
-use crate::mem::MutPtr;
+use crate::mem::{MutPtr, MutVoidPtr, SafeRead};
 use crate::Environment;
 use std::time::SystemTime;
 
@@ -14,6 +14,8 @@ use std::time::SystemTime;
 pub struct State {
     y2k38_warned: bool,
 }
+
+// time.h (C)
 
 #[allow(non_camel_case_types)]
 type time_t = i32;
@@ -34,4 +36,46 @@ fn time(env: &mut Environment, out: MutPtr<time_t>) -> time_t {
     time
 }
 
-pub const FUNCTIONS: FunctionExports = &[export_c_func!(time(_))];
+// sys/time.h (POSIX)
+
+#[allow(non_camel_case_types)]
+type suseconds_t = i32;
+
+#[allow(non_camel_case_types)]
+#[repr(C, packed)]
+struct timeval {
+    tv_sec: time_t,
+    tv_usec: suseconds_t,
+}
+unsafe impl SafeRead for timeval {}
+
+fn gettimeofday(
+    env: &mut Environment,
+    timeval_ptr: MutPtr<timeval>,
+    timezone_ptr: MutVoidPtr, // deprecated, always NULL
+) -> i32 {
+    assert!(timezone_ptr.is_null());
+
+    if timeval_ptr.is_null() {
+        return 0; // success
+    }
+
+    let time = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap();
+
+    let time_s_64: u64 = time.as_secs();
+    let tv_sec = time_s_64 as time_t;
+    if !env.libc_state.time.y2k38_warned && time_s_64 != tv_sec as u64 {
+        env.libc_state.time.y2k38_warned = true;
+        log!("Warning: system clock is beyond Y2K38 and might confuse the app");
+    }
+    let tv_usec: suseconds_t = time.subsec_micros().try_into().unwrap();
+
+    env.mem.write(timeval_ptr, timeval { tv_sec, tv_usec });
+
+    0 // success
+}
+
+pub const FUNCTIONS: FunctionExports =
+    &[export_c_func!(time(_)), export_c_func!(gettimeofday(_, _))];

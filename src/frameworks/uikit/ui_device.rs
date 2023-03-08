9410cc1c343a683db36278d0c492b328d738810f
upintheairsheep <43690204+upintheairsheep@users.noreply.github.com>
2023-03-08 07:18:10 -0800

    Update ui_device.rs

--- src/frameworks/uikit/ui_device.rs
+++ src/frameworks/uikit/ui_device.rs
@@ -60,8 +60,8 @@ pub const CLASSES: ClassExports = objc_classes! {
     ns_string::get_static_str(env, "UIUserInterfaceIdiom.phone")
 }
 
-- (id)systemName {
-    ns_string::get_static_str(env, "iPhoneOS")
+- (id)name {
+    ns_string::get_static_str(env, "touchHLE")
 }
 
 

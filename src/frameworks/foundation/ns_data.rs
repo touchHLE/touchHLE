touchHLE/touchHLE

commit bb766c98a1d0c2d682ae6a97af3e3bebdc595bc7

Implement createFileAtPath:contents:attributes:
This also makes env.mem.bytes_at(Ptr::null(), 0) and its mut counterpart
not panic, which is convenient for this use case and has no downsides in
general.

src/frameworks/foundation/ns_data.rs
@@ -48,6 +48,9 @@ pub const CLASSES: ClassExports = objc_classes! {
     autorelease(env, new)
 }
 
+// Calling the standard `init` is also allowed, in which case we just get data
+// of size 0.
+
 - (id)initWithBytesNoCopy:(MutVoidPtr)bytes
                    length:(NSUInteger)length {
     let host_object = env.objc.borrow_mut::<NSDataHostObject>(this);

src/mem.rs
@@ -312,6 +312,11 @@ impl Mem {
     /// Get a slice for reading `count` bytes. This is the basic primitive for
     /// safe read-only memory access.
     pub fn bytes_at<const MUT: bool>(&self, ptr: Ptr<u8, MUT>, count: GuestUSize) -> &[u8] {
+        // Allow null pointer for zero-length slices. It would be annoying not
+        // to (consider e.g. [[NSData new] bytes] is NULL).
+        if count == 0 {
+            return &[];
+        }
         if ptr.to_bits() < Self::NULL_PAGE_SIZE {
             Self::null_check_fail(ptr.to_bits(), count)
         }
@@ -320,6 +325,11 @@ impl Mem {
     /// Get a slice for reading or writing `count` bytes. This is the basic
     /// primitive for safe read-write memory access.
     pub fn bytes_at_mut(&mut self, ptr: MutPtr<u8>, count: GuestUSize) -> &mut [u8] {
+        // Allow null pointer for zero-length slices. It would be annoying not
+        // to (consider e.g. [[NSData new] bytes] is NULL).
+        if count == 0 {
+            return &mut [];
+        }
         if ptr.to_bits() < Self::NULL_PAGE_SIZE {
             Self::null_check_fail(ptr.to_bits(), count)
         }

src/frameworks/foundation/ns_file_manager.rs
@@ -8,7 +8,7 @@
 use super::{ns_array, ns_string, NSUInteger};
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::fs::GuestPath;
-use crate::objc::{autorelease, id, msg, objc_classes, ClassExports};
+use crate::objc::{autorelease, id, msg, msg_class, nil, objc_classes, release, ClassExports};
 use crate::Environment;
 
 type NSSearchPathDirectory = NSUInteger;
@@ -73,6 +73,28 @@ pub const CLASSES: ClassExports = objc_classes! {
     let res = env.fs.exists(GuestPath::new(&path));
     log_dbg!("fileExistsAtPath:{:?} => {}", path, res);
     res
+}
+
+- (bool)createFileAtPath:(id)path // NSString*
+                contents:(id)data // NSData*
+              attributes:(id)attributes { // NSDictionary*
+    assert!(attributes == nil); // TODO
+
+    let path_str = ns_string::to_rust_string(env, path); // TODO: avoid copy
+    // createFileAtPath: returns true if there's already a file at a given path.
+    // If there's a directory, that's an error, though.
+    if env.fs.is_file(GuestPath::new(&path_str)) {
+        return true;
+    }
+
+    if data == nil {
+        let empty: id = msg_class![env; NSData new];
+        let res: bool = msg![env; empty writeToFile:path atomically:false];
+        release(env, empty);
+        res
+    } else {
+        msg![env; data writeToFile:path atomically:false]
+    }
 }
 
 @end

commit a3e5b37862632d95005be8b906a82e1ca440c2d0

Fix regression in bytes_at()/bytes_at_mut() that broke texturing in SMB
This was introduced by bb766c98a1d0c2d682ae6a97af3e3bebdc595bc7.

Redirecting zero-length accesses to a special empty slice is not safe,
because we can sometimes derive pointers from such slices that we then
access out-of-bounds. Arguably this is fine so long as
ptr_at()/ptr_at_mut() take a different path, but I don't feel like
refactoring this right now.

src/frameworks/foundation/ns_data.rs
@@ -95,7 +95,13 @@ pub const CLASSES: ClassExports = objc_classes! {
     let file = to_rust_string(env, path);
     log_dbg!("[(NSData*){:?} writeToFile:{:?} atomically:_]", this, file);
     let host_object = env.objc.borrow::<NSDataHostObject>(this);
-    let slice = env.mem.bytes_at(host_object.bytes.cast(), host_object.length);
+    // Mem::bytes_at() panics when the pointer is NULL, but NSData's pointer can
+    // be NULL if the length is 0.
+    let slice = if host_object.length == 0 {
+        &[]
+    } else {
+        env.mem.bytes_at(host_object.bytes.cast(), host_object.length)
+    };
     env.fs.write(GuestPath::new(&file), slice).is_ok()
 }
 

src/mem.rs
@@ -311,12 +311,12 @@ impl Mem {
 
     /// Get a slice for reading `count` bytes. This is the basic primitive for
     /// safe read-only memory access.
+    ///
+    /// This will panic when `ptr` is within the null page, even if `count` is
+    /// 0. This may be inconvenient in some cases, but it makes the behavior
+    /// when deriving a pointer from the slice consistent (though you should use
+    /// [Self::ptr_at] for that).
     pub fn bytes_at<const MUT: bool>(&self, ptr: Ptr<u8, MUT>, count: GuestUSize) -> &[u8] {
-        // Allow null pointer for zero-length slices. It would be annoying not
-        // to (consider e.g. [[NSData new] bytes] is NULL).
-        if count == 0 {
-            return &[];
-        }
         if ptr.to_bits() < Self::NULL_PAGE_SIZE {
             Self::null_check_fail(ptr.to_bits(), count)
         }
@@ -324,12 +324,12 @@ impl Mem {
     }
     /// Get a slice for reading or writing `count` bytes. This is the basic
     /// primitive for safe read-write memory access.
+    ///
+    /// This will panic when `ptr` is within the null page, even if `count` is
+    /// 0. This may be inconvenient in some cases, but it makes the behavior
+    /// when deriving a pointer from the slice consistent (though you should use
+    /// [Self::ptr_at_mut] for that).
     pub fn bytes_at_mut(&mut self, ptr: MutPtr<u8>, count: GuestUSize) -> &mut [u8] {
-        // Allow null pointer for zero-length slices. It would be annoying not
-        // to (consider e.g. [[NSData new] bytes] is NULL).
-        if count == 0 {
-            return &mut [];
-        }
         if ptr.to_bits() < Self::NULL_PAGE_SIZE {
             Self::null_check_fail(ptr.to_bits(), count)
         }
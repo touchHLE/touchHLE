c76eeb533789a526e6651a4c3a991e0618cf02e3
hikari_no_yume <hikari@noyu.me>
2023-04-21 15:33:00 +0200

    Remove now-unnecessary #[allow(dead_code)] annotations

--- src/frameworks/core_foundation/cf_allocator.rs
+++ src/frameworks/core_foundation/cf_allocator.rs
@@ -9,10 +9,8 @@ use super::CFTypeRef;
 use crate::dyld::{ConstantExports, HostConstant};
 use crate::mem::Ptr;
 
-#[allow(dead_code)]
 pub type CFAllocatorRef = CFTypeRef;
 
-#[allow(dead_code)]
 pub const kCFAllocatorDefault: CFAllocatorRef = Ptr::null();
 
 pub const CONSTANTS: ConstantExports = &[("_kCFAllocatorDefault", HostConstant::NullPtr)];

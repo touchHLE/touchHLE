a7a42c7ae1e02863f9ccff27635224d21bfd720c
hikari_no_yume <hikari@noyu.me>
2023-05-06 21:07:46 +0200

    Clarify licensing (per-file license header)

--- android/app/src/main/java/org/touchhle/android/MainActivity.java
+++ android/app/src/main/java/org/touchhle/android/MainActivity.java
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Parts of this file are derived from SDL 2's Android project template, which
+ * has a different license. Please see vendor/SDL/LICENSE.txt for details.
+ */
 package org.touchhle.android;
 
 import org.libsdl.app.SDLActivity;

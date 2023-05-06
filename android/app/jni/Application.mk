9bff221909ebd94e1036e8157f1804bf7d57ca45
hikari_no_yume <hikari@noyu.me>
2023-05-06 21:12:47 +0200

    Sync Application.mk with build.gradle (does this mean anything?)

--- android/app/jni/Application.mk
+++ android/app/jni/Application.mk
@@ -10,7 +10,8 @@
 # https://developer.android.com/ndk/guides/cpp-support
 APP_STL := c++_shared
 
-APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+#APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+APP_ABI := arm64-v8a
 
 # Min runtime API level
-APP_PLATFORM=android-16
+APP_PLATFORM=android-21

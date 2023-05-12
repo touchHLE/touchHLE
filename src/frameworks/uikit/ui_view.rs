2d318aa7f7d01eaac8614c758db3a499e0759fcc
ciciplusplus <ciciplusplus@gmail.com>
2023-05-12 22:38:19 +0200

    Various UIKit related stubs

--- src/frameworks/uikit/ui_view.rs
+++ src/frameworks/uikit/ui_view.rs
@@ -124,6 +124,10 @@ pub const CLASSES: ClassExports = objc_classes! {
     this
 }
 
+- (())setUserInteractionEnabled:(bool)_enabled {
+    // TODO: enable user interaction
+}
+
 // TODO: setMultipleTouchEnabled
 - (())setMultipleTouchEnabled:(bool)_enabled {
     // TODO: enable multitouch

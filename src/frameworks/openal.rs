03df4ad199574c2b57978588b3041dc85e2c4ea9
ciciplusplus <ciciplusplus@gmail.com>
2023-05-13 13:22:43 +0200

    Game specifc audio hack: ignoring 0 doppler velocity

--- src/frameworks/openal.rs
+++ src/frameworks/openal.rs
@@ -321,7 +321,17 @@ fn alDopplerFactor(_env: &mut Environment, value: ALfloat) {
     unsafe { al::alDopplerFactor(value) };
 }
 
-fn alDopplerVelocity(_env: &mut Environment, value: ALfloat) {
+fn alDopplerVelocity(env: &mut Environment, value: ALfloat) {
+    // Apparently wolf3d sets doppler velocity to zero, but this results in muting all of the audio
+    // with Open AL 1.1 soft implementation!
+    // Check "A note for OpenAL library implementors regarding OpenAL 1.0" from OpenAL 1.1 specs for more info
+    let bundle_id = env.bundle.bundle_identifier();
+    if bundle_id.starts_with("com.zodttd.wolf3d") || bundle_id.starts_with("com.idsoftware.wolf3d")
+    {
+        log_dbg!("Applying game-specific hack for Wolf3D-iOS: ignoring 0.0 doppler velocity.");
+        assert_eq!(value, 0.0);
+        return;
+    }
     unsafe { al::alDopplerVelocity(value) };
 }
 

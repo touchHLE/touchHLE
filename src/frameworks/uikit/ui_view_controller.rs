90b3aebd7b1c21c79494e06f11e5706c0bae7272
hikari_no_yume <hikari@noyu.me>
2023-08-15 13:58:08 +0200

    Implement view property on UIViewController (no loadView yet)

--- src/frameworks/uikit/ui_view_controller.rs
+++ src/frameworks/uikit/ui_view_controller.rs
@@ -5,13 +5,44 @@
  */
 //! `UIViewController`.
 
-use crate::objc::{objc_classes, ClassExports};
+use crate::objc::{id, nil, objc_classes, release, retain, ClassExports, HostObject, NSZonePtr};
+
+#[derive(Default)]
+struct UIViewControllerHostObject {
+    view: id,
+}
+impl HostObject for UIViewControllerHostObject {}
 
 pub const CLASSES: ClassExports = objc_classes! {
 
 (env, this, _cmd);
 
 @implementation UIViewController: UIResponder
+
++ (id)allocWithZone:(NSZonePtr)_zone {
+    let host_object = Box::<UIViewControllerHostObject>::default();
+    env.objc.alloc_object(this, host_object, &mut env.mem)
+}
+
+- (())dealloc {
+    let &UIViewControllerHostObject { view } = env.objc.borrow(this);
+
+    release(env, view);
+
+    env.objc.dealloc_object(this, &mut env.mem);
+}
+
+- (())setView:(id)new_view { // UIView*
+    let host_obj = env.objc.borrow_mut::<UIViewControllerHostObject>(this);
+    let old_view = std::mem::replace(&mut host_obj.view, new_view);
+    retain(env, new_view);
+    release(env, old_view);
+}
+- (id)view {
+    let view = env.objc.borrow_mut::<UIViewControllerHostObject>(this).view;
+    assert!(view != nil); // TODO: call loadView if nil
+    view
+}
 
 - (())setEditing:(bool)editing {
     log!("TODO: [(UIViewController*){:?} setEditing:{}]", this, editing); // TODO

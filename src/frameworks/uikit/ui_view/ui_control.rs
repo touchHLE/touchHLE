d3f7cdbd0432c967f7fc36604c5b423d21e0bb76
hikari_no_yume <hikari@noyu.me>
2023-08-19 13:59:28 +0200

    Implement state, enabled, selected, highlighted properties of UIControl

--- src/frameworks/uikit/ui_view/ui_control.rs
+++ src/frameworks/uikit/ui_view/ui_control.rs
@@ -9,17 +9,30 @@ pub mod ui_button;
 pub mod ui_text_field;
 
 use crate::frameworks::foundation::NSUInteger;
-use crate::objc::{objc_classes, ClassExports};
+use crate::objc::{objc_classes, ClassExports, impl_HostObject_with_superclass, id, NSZonePtr};
 
-type UIControlHostObject = super::UIViewHostObject;
+struct UIControlHostObject {
+    superclass: super::UIViewHostObject,
+    enabled: bool,
+    selected: bool,
+    highlighted: bool,
+}
+impl_HostObject_with_superclass!(UIControlHostObject);
+impl Default for UIControlHostObject {
+    fn default() -> Self {
+        UIControlHostObject {
+            superclass: Default::default(),
+            enabled: true,
+            selected: false,
+            highlighted: false,
+        }
+    }
+}
 
 type UIControlState = NSUInteger;
 const UIControlStateNormal: UIControlState = 0;
-#[allow(dead_code)]
 const UIControlStateHighlighted: UIControlState = 1 << 0;
-#[allow(dead_code)]
 const UIControlStateDisabled: UIControlState = 1 << 1;
-#[allow(dead_code)]
 const UIControlStateSelected: UIControlState = 1 << 2;
 #[allow(dead_code)]
 const UIControlStateFocused: UIControlState = 1 << 3;
@@ -31,10 +44,53 @@ pub const CLASSES: ClassExports = objc_classes! {
 // abstract class
 @implementation UIControl: UIView
 
++ (id)allocWithZone:(NSZonePtr)_zone {
+    let host_object = Box::<UIControlHostObject>::default();
+    env.objc.alloc_object(this, host_object, &mut env.mem)
+}
+
 // TODO: state, triggers, etc
 
-- (())setEnabled:(bool)_enabled {
-    // TODO
+- (UIControlState)state {
+    let &UIControlHostObject {
+        superclass: _,
+        highlighted,
+        enabled,
+        selected,
+    } = env.objc.borrow(this);
+    // TODO: focussed
+    let mut state = 0; // aka UIControlStateNormal
+    if highlighted {
+        state |= UIControlStateHighlighted;
+    }
+    if !enabled {
+        state |= UIControlStateDisabled;
+    }
+    if selected {
+        state |= UIControlStateSelected;
+    }
+    state
+}
+
+- (bool)isEnabled {
+    env.objc.borrow::<UIControlHostObject>(this).enabled
+}
+- (())setEnabled:(bool)enabled {
+    env.objc.borrow_mut::<UIControlHostObject>(this).enabled = enabled;
+}
+
+- (bool)isSelected {
+    env.objc.borrow::<UIControlHostObject>(this).selected
+}
+- (())setSelected:(bool)selected {
+    env.objc.borrow_mut::<UIControlHostObject>(this).selected = selected;
+}
+
+- (bool)isHighlighted {
+    env.objc.borrow::<UIControlHostObject>(this).highlighted
+}
+- (())setHighlighted:(bool)highlighted {
+    env.objc.borrow_mut::<UIControlHostObject>(this).highlighted = highlighted;
 }
 
 @end

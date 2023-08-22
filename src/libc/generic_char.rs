2412b6809b98d97e105c4c932dd60c8fabcbea52
alborrajo <alvarolopezborr@gmail.com>
2023-08-23 00:59:47 +0200

    strncat implementation
    Change-Id: I2c5676625e29956d048bc5c007a2cba8865a419d

--- src/libc/generic_char.rs
+++ src/libc/generic_char.rs
@@ -11,6 +11,7 @@ use crate::Environment;
 use std::cmp::Ordering;
 use std::fmt::Debug;
 
+
 /// This type is never actually constructed, it just enables us to move all the
 /// bounds on `T` to the `impl` block.
 pub(super) struct GenericChar<T> {
@@ -202,6 +203,19 @@ impl<T: Copy + Default + Eq + Ord + SafeRead + Debug> GenericChar<T> {
                 }
             }
         }
+    }
+
+    pub(super) fn strncat(
+        env: &mut Environment,
+        s1: ConstPtr<T>,
+        s2: ConstPtr<T>,
+        n: GuestUSize,
+    ) -> ConstPtr<T> {
+        {
+            let s1 = s1 + Self::strlen(env, s1);
+            Self::strncpy(env, s1.cast_mut(), s2, n);
+        }
+        s1
     }
 
     pub(super) fn strstr(

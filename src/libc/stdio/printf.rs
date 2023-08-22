0873bb4f2c2c457171ec9b719379548b401de5ff
alborrajo <alvarolopezborr@gmail.com>
2023-08-23 01:06:36 +0200

    Fixed printf  handling of flags for number padding
    Change-Id: Ic42f3ece65a5afe35a76c2961f7acf851eb27f51

--- src/libc/stdio/printf.rs
+++ src/libc/stdio/printf.rs
@@ -43,12 +43,17 @@ pub fn printf_inner<const NS_LOG: bool, F: Fn(&Mem, GuestUSize) -> u8>(
             continue;
         }
 
-        let mut pad_char = if get_format_char(&env.mem, format_char_idx) == b'0' {
+        let flags = get_format_char(&env.mem, format_char_idx);
+        let mut pad_char = if flags == b'0' {
             format_char_idx += 1;
             '0'
+        } else if (b'1'..=b'9').contains(&flags) {
+            format_char_idx += 1;
+            ' '
         } else {
             ' '
         };
+
         let mut has_precision = false;
         if get_format_char(&env.mem, format_char_idx) == b'.' {
             has_precision = true;

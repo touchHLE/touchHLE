f5dc9770726b41a54076c784e889ba3fa45081b9
alborrajo <alvarolopezborr@gmail.com>
2023-08-23 00:50:11 +0200

    Allowing color factors in GLES BlendFunc
    Change-Id: I856ce80474523e38d2bc2ce97da00ce15f534dce

--- src/gles/gles1_on_gl2.rs
+++ src/gles/gles1_on_gl2.rs
@@ -656,7 +656,8 @@ impl GLES for GLES1OnGL2 {
             gl21::ONE_MINUS_SRC_ALPHA,
             gl21::DST_ALPHA,
             gl21::ONE_MINUS_DST_ALPHA,
-            gl21::SRC_ALPHA_SATURATE
+            gl21::SRC_ALPHA_SATURATE,
+            gl21::SRC_COLOR
         ]
         .contains(&sfactor));
         assert!([
@@ -667,7 +668,8 @@ impl GLES for GLES1OnGL2 {
             gl21::SRC_ALPHA,
             gl21::ONE_MINUS_SRC_ALPHA,
             gl21::DST_ALPHA,
-            gl21::ONE_MINUS_DST_ALPHA
+            gl21::ONE_MINUS_DST_ALPHA,
+            gl21::DST_COLOR
         ]
         .contains(&dfactor));
         gl21::BlendFunc(sfactor, dfactor);

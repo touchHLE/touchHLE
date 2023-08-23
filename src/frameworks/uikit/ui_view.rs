touchHLE/touchHLE

commit 3b606c7fbed1f545d3976b2823e8b0b056fe3c7f

UIView init cleanup + set opaque
Moves the common parts of initWithCoder: and initWithFrame: into the
init method, moves layer creation out of allocWithZone: and into init,
and sets the layer to be opaque.

src/frameworks/uikit/ui_view.rs
@@ -29,10 +29,7 @@ pub const CLASSES: ClassExports = objc_classes! {
 @implementation UIView: UIResponder
 
 + (id)allocWithZone:(NSZonePtr)_zone {
-    let layer_class: Class = msg![env; this layerClass];
-    let layer: id = msg![env; layer_class layer];
-
-    let host_object = Box::new(UIViewHostObject { layer });
+    let host_object = Box::new(UIViewHostObject { layer: nil });
     env.objc.alloc_object(this, host_object, &mut env.mem)
 }
 
@@ -42,9 +39,24 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 // TODO: accessors etc
 
-- (id)initWithFrame:(CGRect)frame {
-    let layer = env.objc.borrow::<UIViewHostObject>(this).layer;
+- (id)init {
+    let view_class: Class = msg![env; this class];
+    let layer_class: Class = msg![env; view_class layerClass];
+    let layer: id = msg![env; layer_class layer];
+
+    // CALayer is not opaque by default, but UIView is
     () = msg![env; layer setDelegate:this];
+    () = msg![env; layer setOpaque:true];
+
+    env.objc.borrow_mut::<UIViewHostObject>(this).layer = layer;
+
+    env.framework_state.uikit.ui_view.views.push(this);
+
+    this
+}
+
+- (id)initWithFrame:(CGRect)frame {
+    let this: id = msg![env; this init];
 
     () = msg![env; this setFrame:frame];
 
@@ -56,13 +68,13 @@ pub const CLASSES: ClassExports = objc_classes! {
         { let center: CGPoint = msg![env; this center]; center },
     );
 
-    env.framework_state.uikit.ui_view.views.push(this);
-
     this
 }
 
 // NSCoding implementation
 - (id)initWithCoder:(id)coder {
+    let this: id = msg![env; this init];
+
     // TODO: decode the various other UIView properties
 
     let key_ns_string = get_static_str(env, "UIBounds");
@@ -79,13 +91,8 @@ pub const CLASSES: ClassExports = objc_classes! {
         center
     );
 
-    let layer = env.objc.borrow::<UIViewHostObject>(this).layer;
-    () = msg![env; layer setDelegate:this];
-
     () = msg![env; this setBounds:bounds];
     () = msg![env; this setCenter:center];
-
-    env.framework_state.uikit.ui_view.views.push(this);
 
     this
 }

commit 7e6c9f850f6fb30ba503ca46c185a9e2184f36fe

Fix initializer chains in UIView (do not call init from initWithFrame:)
3b606c7fbed1f545d3976b2823e8b0b056fe3c7f accidentally caused a
regression in Mystery Mania, because that game has a view that
super-calls initWithCoder:, which then called init, which was
overridden by the class.

This commit changes UIView and all its subclasses to be more careful
in their constructors. Constructors should never make ordinary calls,
only supercalls, and for UIView, the only valid supercalls should be
initWithCoder: and initWithFrame:.

src/frameworks/uikit/ui_view.rs
@@ -22,6 +22,7 @@ use crate::frameworks::foundation::NSUInteger;
 use crate::objc::{
     id, msg, nil, objc_classes, release, retain, Class, ClassExports, HostObject, NSZonePtr,
 };
+use crate::Environment;
 
 #[derive(Default)]
 pub struct State {
@@ -55,6 +56,27 @@ impl Default for UIViewHostObject {
     }
 }
 
+/// Shared parts of `initWithCoder:` and `initWithFrame:`. These can't call
+/// `init`: the subclass may have overridden `init` and will not expect to be
+/// called here.
+///
+/// Do not call this in subclasses of `UIView`.
+fn init_common(env: &mut Environment, this: id) -> id {
+    let view_class: Class = msg![env; this class];
+    let layer_class: Class = msg![env; view_class layerClass];
+    let layer: id = msg![env; layer_class layer];
+
+    // CALayer is not opaque by default, but UIView is
+    () = msg![env; layer setDelegate:this];
+    () = msg![env; layer setOpaque:true];
+
+    env.objc.borrow_mut::<UIViewHostObject>(this).layer = layer;
+
+    env.framework_state.uikit.ui_view.views.push(this);
+
+    this
+}
+
 pub const CLASSES: ClassExports = objc_classes! {
 
 (env, this, _cmd);
@@ -72,24 +94,16 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 // TODO: accessors etc
 
+// initWithCoder: and initWithFrame: are basically UIView's designated
+// initializers. init is not, it's a shortcut for the latter.
+// Subclasses need to override both.
+
 - (id)init {
-    let view_class: Class = msg![env; this class];
-    let layer_class: Class = msg![env; view_class layerClass];
-    let layer: id = msg![env; layer_class layer];
-
-    // CALayer is not opaque by default, but UIView is
-    () = msg![env; layer setDelegate:this];
-    () = msg![env; layer setOpaque:true];
-
-    env.objc.borrow_mut::<UIViewHostObject>(this).layer = layer;
-
-    env.framework_state.uikit.ui_view.views.push(this);
-
-    this
+    msg![env; this initWithFrame:(<CGRect as Default>::default())]
 }
 
 - (id)initWithFrame:(CGRect)frame {
-    let this: id = msg![env; this init];
+    let this = init_common(env, this);
 
     () = msg![env; this setFrame:frame];
 
@@ -106,7 +120,7 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 // NSCoding implementation
 - (id)initWithCoder:(id)coder {
-    let this: id = msg![env; this init];
+    let this = init_common(env, this);
 
     // TODO: decode the various other UIView properties
 

src/frameworks/uikit/ui_view/ui_control/ui_button.rs
@@ -103,8 +103,8 @@ pub const CLASSES: ClassExports = objc_classes! {
     autorelease(env, button)
 }
 
-- (id)init {
-    let this: id = msg_super![env; this init];
+- (id)initWithFrame:(CGRect)frame {
+    let this: id = msg_super![env; this initWithFrame:frame];
 
     () = msg![env; this setOpaque:false];
     let bg_color: id = msg_class![env; UIColor clearColor];
@@ -132,6 +132,8 @@ pub const CLASSES: ClassExports = objc_classes! {
 
     this
 }
+
+// TODO: initWithCoder:
 
 - (())dealloc {
     let UIButtonHostObject {

src/frameworks/uikit/ui_view/ui_image_view.rs
@@ -31,8 +31,8 @@ pub const CLASSES: ClassExports = objc_classes! {
     env.objc.alloc_object(this, host_object, &mut env.mem)
 }
 
-- (id)init {
-    let this: id = msg_super![env; this init];
+- (id)initWithFrame:(CGRect)frame {
+    let this: id = msg_super![env; this initWithFrame:frame];
     // Not sure if UIImageView does this unconditionally, or only for images
     // with alpha channels.
     () = msg![env; this setOpaque:false];
@@ -56,8 +56,11 @@ pub const CLASSES: ClassExports = objc_classes! {
         origin: CGPoint { x: 0.0, y: 0.0 },
         size
     };
-    let this = msg![env; this initWithFrame:frame];
+    let this = msg_super![env; this initWithFrame:frame];
     () = msg![env; this setImage:image];
+    // Not sure if UIImageView does this unconditionally, or only for images
+    // with alpha channels.
+    () = msg![env; this setOpaque:false];
     this
 }
 

src/frameworks/uikit/ui_view/ui_label.rs
@@ -57,8 +57,8 @@ pub const CLASSES: ClassExports = objc_classes! {
     env.objc.alloc_object(this, host_object, &mut env.mem)
 }
 
-- (id)init {
-    let this: id = msg_super![env; this init];
+- (id)initWithFrame:(CGRect)frame {
+    let this: id = msg_super![env; this initWithFrame:frame];
     // These aren't redundant, the setters fetch the real defaults.
     () = msg![env; this setFont:nil];
     () = msg![env; this setTextColor:nil];

src/frameworks/uikit/ui_view/ui_window.rs
@@ -5,6 +5,7 @@
  */
 //! `UIWindow`.
 
+use crate::frameworks::core_graphics::CGRect;
 use crate::objc::{id, msg, msg_super, objc_classes, ClassExports};
 
 #[derive(Default)]
@@ -23,9 +24,9 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 // TODO: more?
 
-- (id)init {
-    let this: id = msg_super![env; this init];
-
+- (id)initWithFrame:(CGRect)frame {
+    // setHidden: may get called during the super call and panics if the window
+    // is not in the list, so it must be added to it before that call.
     let visible_list = &mut env.framework_state.uikit.ui_view.ui_window.visible_windows;
     visible_list.push(this);
     log_dbg!(
@@ -34,7 +35,22 @@ pub const CLASSES: ClassExports = objc_classes! {
         visible_list,
     );
 
-    this
+    msg_super![env; this initWithFrame:frame]
+}
+
+// NSCoding implementation
+- (id)initWithCoder:(id)coder {
+    // setHidden: may get called during the super call and panics if the window
+    // is not in the list, so it must be added to it before that call.
+    let visible_list = &mut env.framework_state.uikit.ui_view.ui_window.visible_windows;
+    visible_list.push(this);
+    log_dbg!(
+        "New window: {:?}. New set of visible windows: {:?}",
+        this,
+        visible_list,
+    );
+
+    msg_super![env; this initWithCoder:coder]
 }
 
 - (())dealloc {
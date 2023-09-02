a103e5057e28c764c6f1a764e871b895f4d1dd29
alborrajo <alvarolopezborr@gmail.com>
2023-09-02 17:01:56 +0200

    Add Clips to bounds stubs to UIView
    Change-Id: Id3245050ba589134511bfe6cb72d99223eb648d2

--- src/frameworks/uikit/ui_view/ui_scroll_view.rs
+++ src/frameworks/uikit/ui_view/ui_scroll_view.rs
@@ -93,6 +93,12 @@ pub const CLASSES: ClassExports = objc_classes! {
     log!("TODO: [(UIScrollView*) {:?} setIndicatorStyle:{:?}]", this, style);
 }
 
+- (bool)isClipsToBounds {
+    // TODO: Remove once clips to bounds is implemented properly
+    // and use "true" as the default value.
+    true
+}
+
 - (())touchesMoved:(id)touches // NSSet* of UITouch*
          withEvent:(id)_event { // UIEvent*
     let scroll_enabled: bool = msg![env; this scrollEnabled];

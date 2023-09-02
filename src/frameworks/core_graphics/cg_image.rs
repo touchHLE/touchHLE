e503183d6bbd5b258dfb45e67407861cbf415ce7
alborrajo <alvarolopezborr@gmail.com>
2023-09-02 16:49:09 +0200

    CGImageGetBitsPerPixel stub
    Change-Id: I09d8afb17bc49d236e3492b938a1ecf2abee8c1a

--- src/frameworks/core_graphics/cg_image.rs
+++ src/frameworks/core_graphics/cg_image.rs
@@ -144,6 +144,10 @@ fn CGImageGetHeight(env: &mut Environment, image: CGImageRef) -> GuestUSize {
         .dimensions();
     height
 }
+fn CGImageGetBitsPerPixel(_env: &mut Environment, image: CGImageRef) -> GuestUSize {
+    log!("TODO: CGImageGetBitsPerPixel({:?})", image);
+    24 // Since we're assuming sRGB
+}
 
 fn CGImageGetDataProvider(env: &mut Environment, image: CGImageRef) -> CGDataProviderRef {
     // CGImageGetDataProvider() seems to be intended to return the underlying
@@ -169,6 +173,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CGImageGetColorSpace(_)),
     export_c_func!(CGImageGetWidth(_)),
     export_c_func!(CGImageGetHeight(_)),
+    export_c_func!(CGImageGetBitsPerPixel(_)),
     export_c_func!(CGImageGetDataProvider(_)),
     export_c_func!(CGImageGetBitsPerComponent(_)),
 ];

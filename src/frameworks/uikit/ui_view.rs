f9d9609964516ecea433ad2b8942342f1ad0c1ba
alborrajo <alvarolopezborr@gmail.com>
2023-09-02 17:01:56 +0200

    Add Clips to bounds stubs to UIView
    Change-Id: Id3245050ba589134511bfe6cb72d99223eb648d2

--- src/frameworks/uikit/ui_view.rs
+++ src/frameworks/uikit/ui_view.rs
@@ -664,6 +664,9 @@ pub const CLASSES: ClassExports = objc_classes! {
     msg![env; layer setHidden:hidden]
 }
 
+- (bool)isClipsToBounds {
+    false
+}
 - (())setClipsToBounds:(bool)clips {
     log!("TODO: [{:?} setClipsToBounds:{}]", this, clips);
 }

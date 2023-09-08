b14ec76e1bf7419e3375055e572439e4e8b7f47f
ciciplusplus <ciciplusplus@gmail.com>
2023-09-08 13:13:14 +0300

    Network related stubs
    Change-Id: I470e07bfa3c0072a6f11834cc7fbcd4bcc4e54c3

---
+++ src/libc/ifaddrs.rs
@@ -0,0 +1,22 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+//! `ifaddrs.h` (interface addresses)
+
+use crate::dyld::FunctionExports;
+use crate::export_c_func;
+use crate::mem::MutPtr;
+use crate::Environment;
+
+// TODO: struct definition
+#[allow(non_camel_case_types)]
+struct ifaddrs {}
+
+fn getifaddrs(_env: &mut Environment, _ifap: MutPtr<MutPtr<ifaddrs>>) -> i32 {
+    // TODO: implement
+    -1
+}
+
+pub const FUNCTIONS: FunctionExports = &[export_c_func!(getifaddrs(_))];

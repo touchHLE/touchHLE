d9b955efd2d1efde3ddd56110469f21378d3bac3
ciciplusplus <ciciplusplus@gmail.com>
2023-10-12 20:28:02 +0200

    Per-thread pool stacks for NSAutoreleasePool
    Also fix introduced by this change crash in the app_picker
    
    Change-Id: I047c5f2955bf075859d184a55d1c9cf978460284

--- src/frameworks/foundation/ns_autorelease_pool.rs
+++ src/frameworks/foundation/ns_autorelease_pool.rs
@@ -6,11 +6,12 @@
 //! `NSAutoreleasePool`.
 
 use crate::objc::{id, msg, objc_classes, release, ClassExports, HostObject, NSZonePtr};
-use crate::Environment;
+use crate::{Environment, ThreadId};
+use std::collections::HashMap;
 
 #[derive(Default)]
 pub struct State {
-    pool_stack: Vec<id>,
+    pool_stacks: HashMap<ThreadId, Vec<id>>,
 }
 impl State {
     fn get(env: &mut Environment) -> &mut Self {
@@ -19,6 +20,7 @@ impl State {
 }
 
 struct NSAutoreleasePoolHostObject {
+    original_thread: ThreadId,
     /// This is allowed to contain duplicates, which get released several times!
     objects: Vec<id>,
 }
@@ -32,22 +34,28 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 + (id)allocWithZone:(NSZonePtr)_zone {
     let host_object = Box::new(NSAutoreleasePoolHostObject {
+        original_thread: env.current_thread,
         objects: Vec::new(),
     });
     env.objc.alloc_object(this, host_object, &mut env.mem)
 }
 
 + (())addObject:(id)obj {
-    if let Some(current_pool) = State::get(env).pool_stack.last().copied() {
+    let current_thread = env.current_thread;
+    let pool_stack = State::get(env).pool_stacks.get(&current_thread).unwrap();
+    if let Some(current_pool) = pool_stack.last().copied() {
         msg![env; current_pool addObject:obj]
     } else {
-        log_dbg!("Warning: no active NSAutoreleasePool, leaking {:?}", obj);
+        log_dbg!("Warning: no active NSAutoreleasePool, leaking {:?}, current thread {}", obj, current_thread);
     }
 }
 
 - (id)init {
-    assert!(env.current_thread == 0); // TODO: per-thread stacks
-    State::get(env).pool_stack.push(this);
+    let current_thread = env.current_thread;
+    let pool_stack = State::get(env).pool_stacks
+        .entry(current_thread)
+        .or_default();
+    pool_stack.push(this);
     log_dbg!("New pool: {:?}", this);
     this
 }
@@ -71,9 +79,19 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 - (())dealloc {
     log_dbg!("Draining pool: {:?}", this);
-    let pop_res = State::get(env).pool_stack.pop();
-    assert!(pop_res == Some(this));
+    let current_thread = env.current_thread;
     let host_obj: &mut NSAutoreleasePoolHostObject = env.objc.borrow_mut(this);
+    // It's unclear that should happen when draining a pool on the wrong thread,
+    // but we prefer to be conservative here
+    assert_eq!(host_obj.original_thread, current_thread);
+    let pool_stack = &mut env
+        .framework_state
+        .foundation
+        .ns_autorelease_pool
+        .pool_stacks
+        .get_mut(&current_thread).unwrap();
+    let pop_res = pool_stack.pop();
+    assert!(pop_res == Some(this));
     let objects = std::mem::take(&mut host_obj.objects);
     env.objc.dealloc_object(this, &mut env.mem);
     for object in objects {

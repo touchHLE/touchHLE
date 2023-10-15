00e437d38474733a807b7f9ec3fd18794bc80867
WhatAmISupposedToPutHere <7d578vix8hzw@opayq.net>
2023-10-15 23:10:19 +0200

    Add ftruncate()
    Change-Id: Ia82bea44a0cf444cbb463af8de3fb862255e7734

--- src/libc/posix_io.rs
+++ src/libc/posix_io.rs
@@ -415,6 +415,14 @@ fn flock(_env: &mut Environment, fd: FileDescriptor, operation: FLockFlag) -> i3
     0
 }
 
+fn ftruncate(env: &mut Environment, fd: FileDescriptor, len: off_t) -> i32 {
+    let file = env.libc_state.posix_io.file_for_fd(fd).unwrap();
+    match file.file.set_len(len as u64) {
+        Ok(()) => 0,
+        Err(_) => -1, // TODO: set errno
+    }
+}
+
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(open(_, _, _)),
     export_c_func!(read(_, _, _)),
@@ -425,4 +433,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(getcwd(_, _)),
     export_c_func!(chdir(_)),
     export_c_func!(flock(_, _)),
+    export_c_func!(ftruncate(_, _)),
 ];

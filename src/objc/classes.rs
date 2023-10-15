46bc593a93269e99e804ff83797e24d5706936e9
WhatAmISupposedToPutHere <7d578vix8hzw@opayq.net>
2023-10-15 23:10:18 +0200

    Add conformsToProtocol
    Change-Id: I0db56c1851a4403b8c39bf5c49535d9ac90c0063

--- src/objc/classes.rs
+++ src/objc/classes.rs
@@ -16,9 +16,11 @@ pub(super) use class_lists::CLASS_LISTS;
 use super::{
     id, method_list_t, nil, objc_object, AnyHostObject, HostIMP, HostObject, ObjC, IMP, SEL,
 };
+use crate::environment::Environment;
 use crate::mach_o::MachO;
 use crate::mem::{guest_size_of, ConstPtr, ConstVoidPtr, GuestUSize, Mem, Ptr, SafeRead};
-use std::collections::HashMap;
+use crate::objc::protocols::{collect_protocols_from_bin, protocol_list_t, protocol_t};
+use std::collections::{HashMap, HashSet};
 
 /// Generic pointer to an Objective-C class or metaclass.
 ///
@@ -38,6 +40,7 @@ pub(super) struct ClassHostObject {
     pub(super) is_metaclass: bool,
     pub(super) superclass: Class,
     pub(super) methods: HashMap<SEL, IMP>,
+    pub(super) protocols: HashSet<ConstPtr<protocol_t>>,
     /// Offset into the allocated memory for the object where the ivars of
     /// instances of this class or metaclass (respectively: normal objects or
     /// classes) should live. This is always >= the value in the superclass.
@@ -93,8 +96,8 @@ struct class_rw_t {
     _reserved: u32,
     name: ConstPtr<u8>,
     base_methods: ConstPtr<method_list_t>,
-    _base_protocols: ConstVoidPtr, // protocol list (TODO)
-    _ivars: ConstVoidPtr,          // ivar list (TODO)
+    base_protocols: ConstPtr<protocol_list_t>, // protocol list
+    _ivars: ConstVoidPtr,                      // ivar list (TODO)
     _weak_ivar_layout: u32,
     _base_properties: ConstVoidPtr, // property list (TODO)
 }
@@ -357,6 +360,7 @@ impl ClassHostObject {
                     (objc.selectors[name], IMP::Host(host_imp))
                 }),
             ),
+            protocols: HashSet::new(),
             // maybe this should be 0 for NSObject? does it matter?
             _instance_start: size,
             instance_size: size,
@@ -372,6 +376,7 @@ impl ClassHostObject {
             instance_size,
             name,
             base_methods,
+            base_protocols,
             ..
         } = mem.read(data);
 
@@ -384,10 +389,16 @@ impl ClassHostObject {
             methods: HashMap::new(),
             _instance_start: instance_start,
             instance_size,
+            protocols: HashSet::new(),
         };
 
         if !base_methods.is_null() {
             host_object.add_methods_from_bin(base_methods, mem, objc);
+        }
+        if !base_protocols.is_null() {
+            host_object
+                .protocols
+                .extend(collect_protocols_from_bin(base_protocols, mem));
         }
 
         host_object
@@ -657,6 +668,7 @@ impl ObjC {
                         methods: Default::default(),
                         _instance_start: Default::default(),
                         instance_size: Default::default(),
+                        protocols: Default::default(),
                     },
                 );
                 log_dbg!(
@@ -714,4 +726,9 @@ impl ObjC {
             panic!();
         }
     }
+}
+
+pub fn class_conformsToProtocol(env: &mut Environment, cls: Class, proto: id) -> bool {
+    let host_object: &ClassHostObject = env.objc.borrow(cls);
+    host_object.protocols.contains(&proto.cast_const().cast())
 }

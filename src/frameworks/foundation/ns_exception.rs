b23fd94d7e1bc92f6a475765dd94adc150cb4f4c
WhatAmISupposedToPutHere <7d578vix8hzw@opayq.net>
2023-10-15 23:10:17 +0200

    Add NSSetUncaughtExceptionHandler
    Change-Id: I318feb2688397d0d7fd085a622064fff997c0845

---
+++ src/frameworks/foundation/ns_exception.rs
@@ -0,0 +1,18 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use crate::Environment;
+use crate::mem::MutVoidPtr;
+use crate::dyld::{export_c_func, FunctionExports};
+
+fn NSSetUncaughtExceptionHandler(
+    _: &mut Environment,
+    handler: MutVoidPtr, // void (NSException *)()
+) {
+    log!("TODO: NSSetUncaughtExceptionHandler({:?})", handler);
+}
+
+pub const FUNCTIONS: FunctionExports = &[export_c_func!(NSSetUncaughtExceptionHandler(_))];

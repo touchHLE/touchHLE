9abbf0bf2b2cf04ce6419eb673ae6e277ae12176
WhatAmISupposedToPutHere <7d578vix8hzw@opayq.net>
2023-10-15 23:10:18 +0200

    Add longjmp()
    Change-Id: Ifb2ee6ee6bba20313c9ad27731c4782d74f743ea

--- src/libc/setjmp.rs
+++ src/libc/setjmp.rs
@@ -16,15 +16,72 @@
 //! functions, at least for the former.
 
 use crate::dyld::{export_c_func, FunctionExports};
-use crate::Environment;
+use crate::{abi, Environment};
+use crate::abi::GuestFunction;
+use crate::mem::{MutPtr, SafeRead};
+
+#[repr(C, packed)]
+#[derive(Debug)]
+struct JmpBuf {
+    r4: u32,
+    r5: u32,
+    r6: u32,
+    fp: u32,
+    r8: u32,
+    r10: u32,
+    r11: u32,
+    sp: u32,
+    lr: u32
+}
+
+unsafe impl SafeRead for JmpBuf {}
 
 /// The signature of this is incomplete because it's a stub (see module docs).
-fn setjmp(env: &mut Environment) -> i32 {
+fn setjmp(env: &mut Environment, jmp_buf: MutPtr<JmpBuf>) -> i32 {
+    let lr = env.cpu.regs()[crate::cpu::Cpu::LR];
     log_dbg!(
         "TODO: setjmp() at {:#x}",
-        env.cpu.regs()[crate::cpu::Cpu::LR]
+        lr
     );
+    let buf = JmpBuf {
+        r4: env.cpu.regs()[4],
+        r5: env.cpu.regs()[5],
+        r6: env.cpu.regs()[6],
+        fp: env.cpu.regs()[abi::FRAME_POINTER],
+        r8: env.cpu.regs()[8],
+        r10: env.cpu.regs()[10],
+        r11: env.cpu.regs()[11],
+        sp: env.cpu.regs()[crate::cpu::Cpu::SP],
+        lr: env.cpu.regs()[crate::cpu::Cpu::LR],
+    };
+    env.mem.write(jmp_buf, buf);
     0 // no longjmp() was performed
 }
 
-pub const FUNCTIONS: FunctionExports = &[export_c_func!(setjmp())];
+fn longjmp(env: &mut Environment, jmp_buf: MutPtr<JmpBuf>, status: u32) {
+    let lr = env.cpu.regs()[crate::cpu::Cpu::LR];
+    let fp = env.cpu.regs()[abi::FRAME_POINTER];
+
+    let buf = env.mem.read(jmp_buf);
+    let cur_stack = env.stack_for_longjmp(lr, fp);
+    let other_stack = env.stack_for_longjmp(buf.lr, buf.fp);
+    if cur_stack.last().unwrap() != other_stack.last().unwrap() {
+        unimplemented!("Attempted a longjump across host stack frames");
+    }
+    env.cpu.regs_mut()[0] = status;
+    env.cpu.regs_mut()[4] = buf.r4;
+    env.cpu.regs_mut()[5] = buf.r5;
+    env.cpu.regs_mut()[6] = buf.r6;
+    env.cpu.regs_mut()[abi::FRAME_POINTER] = buf.fp;
+    env.cpu.regs_mut()[8] = buf.r8;
+    env.cpu.regs_mut()[10] = buf.r10;
+    env.cpu.regs_mut()[11] = buf.r11;
+    env.cpu.regs_mut()[crate::cpu::Cpu::SP] = buf.sp;
+    env.cpu.regs_mut()[crate::cpu::Cpu::LR] = buf.lr;
+    env.cpu.branch(GuestFunction::from_addr_with_thumb_bit(buf.lr));
+}
+
+pub const FUNCTIONS: FunctionExports = &[
+    export_c_func!(setjmp(_)),
+    export_c_func!(longjmp(_, _))
+];

5cb8d276d9e55e4e4b55861c045a68a0942512a7
WhatAmISupposedToPutHere <7d578vix8hzw@opayq.net>
2023-10-20 23:26:13 +0200

    Add NSLock
    Change-Id: Id1f369e262410941b1eee311e512fb5329640cff

---
+++ src/frameworks/foundation/ns_lock.rs
@@ -0,0 +1,47 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+
+use crate::environment::{MutexId, MutexType};
+use crate::objc::{ClassExports, HostObject, id, NSZonePtr};
+use crate::objc_classes;
+
+struct LockHostObject {
+    mutex_id: MutexId,
+}
+impl HostObject for LockHostObject {}
+
+pub const CLASSES: ClassExports = objc_classes! {
+
+(env, this, _cmd);
+
+@implementation NSLock: NSObject
+
++ (id)allocWithZone:(NSZonePtr)_zone {
+    let host_object = Box::new(LockHostObject {
+        mutex_id: env.mutex_state.init_mutex(MutexType::PTHREAD_MUTEX_NORMAL),
+    });
+    env.objc.alloc_object(this, host_object, &mut env.mem)
+}
+
+-(())lock {
+    let host_object = env.objc.borrow::<LockHostObject>(this);
+    env.lock_mutex(host_object.mutex_id).unwrap();
+}
+
+-(())unlock {
+    let host_object = env.objc.borrow::<LockHostObject>(this);
+    env.unlock_mutex(host_object.mutex_id).unwrap();
+}
+
+-(())dealloc {
+    let host_object = env.objc.borrow_mut::<LockHostObject>(this);
+    env.mutex_state.destroy_mutex(host_object.mutex_id).unwrap();
+
+    env.objc.dealloc_object(this, &mut env.mem)
+}
+@end
+};

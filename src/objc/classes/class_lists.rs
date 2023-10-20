7a29440543c5bdc541a18007f622cb20d3e4a4cf
ciciplusplus <ciciplusplus@gmail.com>
2023-10-21 01:09:34 +0200

    AVAudioPlayer implementation atop of Audio Queue Services
    Change-Id: I339784d03ac21ccfd365db719c2c387185c1a502

--- src/objc/classes/class_lists.rs
+++ src/objc/classes/class_lists.rs
@@ -6,7 +6,9 @@
 //! Separate module just for the class lists, since this will probably be a
 //! very long and frequently-updated list.
 
-use crate::frameworks::{core_animation, core_graphics, foundation, media_player, opengles, uikit};
+use crate::frameworks::{
+    av_audio, core_animation, core_graphics, foundation, media_player, opengles, uikit,
+};
 
 /// All the lists of classes that the runtime should search through.
 pub const CLASS_LISTS: &[super::ClassExports] = &[
@@ -43,6 +45,7 @@ pub const CLASS_LISTS: &[super::ClassExports] = &[
     foundation::ns_url::CLASSES,
     foundation::ns_user_defaults::CLASSES,
     foundation::ns_value::CLASSES,
+    av_audio::av_audio_player::CLASSES,
     media_player::movie_player::CLASSES,
     opengles::eagl::CLASSES,
     uikit::ui_accelerometer::CLASSES,

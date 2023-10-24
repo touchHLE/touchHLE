d963ff8a14928c64f76c9633e6282be354377ed5
WhatAmISupposedToPutHere <7d578vix8hzw@opayq.net>
2023-10-24 17:45:37 +0200

    Add AVAudioSession
    Change-Id: I7b2899a4033fc64600c64050edcad9bbb06b336b

--- src/objc/classes/class_lists.rs
+++ src/objc/classes/class_lists.rs
@@ -6,11 +6,14 @@
 //! Separate module just for the class lists, since this will probably be a
 //! very long and frequently-updated list.
 
-use crate::frameworks::{core_animation, core_graphics, foundation, media_player, opengles, uikit};
+use crate::frameworks::{
+    avf_audio, core_animation, core_graphics, foundation, media_player, opengles, uikit,
+};
 
 /// All the lists of classes that the runtime should search through.
 pub const CLASS_LISTS: &[super::ClassExports] = &[
     crate::app_picker::CLASSES, // Not a framework! Special internal classes.
+    avf_audio::av_audio_session::CLASSES,
     core_animation::ca_eagl_layer::CLASSES,
     core_animation::ca_layer::CLASSES,
     core_graphics::cg_data_provider::CLASSES,

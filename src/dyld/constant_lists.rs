0fb9daee309141ac459f685e051b9f5604da0756
WhatAmISupposedToPutHere <7d578vix8hzw@opayq.net>
2023-10-24 17:45:37 +0200

    Add AVAudioSession
    Change-Id: I7b2899a4033fc64600c64050edcad9bbb06b336b

--- src/dyld/constant_lists.rs
+++ src/dyld/constant_lists.rs
@@ -6,13 +6,15 @@
 //! Separate module just for the constant lists, since this will probably be a
 //! very long and frequently-updated list.
 
-use crate::frameworks::{core_foundation, core_graphics, foundation, media_player, opengles, uikit};
+
+use crate::frameworks::{avf_audio, core_foundation, core_graphics, foundation, media_player, opengles, uikit};
 use crate::libc;
 
 /// All the lists of constants that the linker should search through.
 pub const CONSTANT_LISTS: &[super::ConstantExports] = &[
     libc::ctype::CONSTANTS,
     libc::stdio::CONSTANTS,
+    avf_audio::av_audio_session::CONSTANTS,
     core_foundation::cf_allocator::CONSTANTS,
     core_foundation::cf_run_loop::CONSTANTS,
     core_graphics::cg_affine_transform::CONSTANTS,

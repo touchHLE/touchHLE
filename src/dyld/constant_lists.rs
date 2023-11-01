320d68dea66ed4e5c480a267fd62736b33388d8e
alborrajo <alvarolopezborr@gmail.com>
2023-11-01 23:31:39 +0100

    UIDevice orientation implementation
    Change-Id: Ifd87336209b09ddab45221a5c4da443af4e9dcb8

--- src/dyld/constant_lists.rs
+++ src/dyld/constant_lists.rs
@@ -6,7 +6,7 @@
 //! Separate module just for the constant lists, since this will probably be a
 //! very long and frequently-updated list.
 
-use crate::frameworks::{core_foundation, core_graphics, foundation, media_player, opengles};
+use crate::frameworks::{self, core_foundation, core_graphics, foundation, media_player, opengles};
 use crate::libc;
 
 /// All the lists of constants that the linker should search through.
@@ -20,6 +20,7 @@ pub const CONSTANT_LISTS: &[super::ConstantExports] = &[
     core_graphics::cg_geometry::CONSTANTS,
     foundation::ns_exception::CONSTANTS,
     foundation::ns_locale::CONSTANTS,
+    frameworks::uikit::ui_device::CONSTANTS,
     foundation::ns_run_loop::CONSTANTS,
     media_player::movie_player::CONSTANTS,
     opengles::eagl::CONSTANTS,

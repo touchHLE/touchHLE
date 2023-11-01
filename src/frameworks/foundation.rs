57f4a4f8ed8d7fd42138c847d5bd8bac4cb74117
alborrajo <alvarolopezborr@gmail.com>
2023-11-01 23:33:52 +0100

    NSFileHandle implementation
    Change-Id: Iae82fcd1ba8b49ddbc93e7b896eb03ce52601518

--- src/frameworks/foundation.rs
+++ src/frameworks/foundation.rs
@@ -23,6 +23,7 @@ pub mod ns_dictionary;
 pub mod ns_enumerator;
 pub mod ns_error;
 pub mod ns_exception;
+pub mod ns_file_handle;
 pub mod ns_file_manager;
 pub mod ns_keyed_unarchiver;
 pub mod ns_locale;

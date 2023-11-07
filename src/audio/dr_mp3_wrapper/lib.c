38631fdbcb220c5d42827a3225d21c39774a9633
hikari_no_yume <hikari@noyu.me>
2023-11-07 19:40:39 +0100

    clang-format dr_mp3_wrapper
    Change-Id: I3acdb25ce02174bd6d6411a192b3579322864317

--- src/audio/dr_mp3_wrapper/lib.c
+++ src/audio/dr_mp3_wrapper/lib.c
@@ -11,8 +11,7 @@
 #include <stdlib.h>
 
 int16_t *touchHLE_decode_mp3_to_pcm(const uint8_t *data, size_t data_size,
-                                    uint32_t *channels,
-                                    uint32_t *sample_rate,
+                                    uint32_t *channels, uint32_t *sample_rate,
                                     uint64_t *frame_count) {
   drmp3_config config;
   int16_t *samples = drmp3_open_memory_and_read_pcm_frames_s16(

8a0ac45f388c1e33e1c56ced358ff7070919bb39
Arlindo Pereira <arlindosaraivapereira@gmail.com>
2023-11-16 11:31:54 +0100

    Add Start button support + couple of default options
    Includes:
    - Start button support
    - buttons support for THPS2
    - adds Start default buttons to:
      - SMB
      - CBNK
      - Fastlane (as well as Camera button)
      - KataDama
    - adds default tilt offset for:
      - KataDama
    - adds options for new game
      - Head2Head
    
    Change-Id: I160592c5b9233937f3e760dec5b439a6c0472383

--- src/window.rs
+++ src/window.rs
@@ -356,6 +356,7 @@ impl Window {
                 sdl2::controller::Button::DPadUp => Some(crate::options::Button::DPadUp),
                 sdl2::controller::Button::DPadRight => Some(crate::options::Button::DPadRight),
                 sdl2::controller::Button::DPadDown => Some(crate::options::Button::DPadDown),
+                sdl2::controller::Button::Start => Some(crate::options::Button::Start),
                 sdl2::controller::Button::A => Some(crate::options::Button::A),
                 sdl2::controller::Button::B => Some(crate::options::Button::B),
                 sdl2::controller::Button::X => Some(crate::options::Button::X),

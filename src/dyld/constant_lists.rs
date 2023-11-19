ed1260a14505a5183cb25472da00c423dd5eeb54
ciciplusplus <ciciplusplus@gmail.com>
2023-11-19 18:56:29 +0100

    Implement unarchiveObjectWithData: of NSKeyedUnarchiver
    Plus refactor some related code
    
    Change-Id: I453f74217d2704c173d2eeece2e22523800c9075

--- src/dyld/constant_lists.rs
+++ src/dyld/constant_lists.rs
@@ -21,6 +21,7 @@ pub const CONSTANT_LISTS: &[super::ConstantExports] = &[
     core_graphics::cg_color_space::CONSTANTS,
     core_graphics::cg_geometry::CONSTANTS,
     foundation::ns_exception::CONSTANTS,
+    foundation::ns_keyed_unarchiver::CONSTANTS,
     foundation::ns_locale::CONSTANTS,
     foundation::ns_run_loop::CONSTANTS,
     media_player::movie_player::CONSTANTS,

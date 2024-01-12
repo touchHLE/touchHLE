1077bd2bf3d67dfab8e9756c295ad5f885912e9d
alborrajo <alvarolopezborr@gmail.com>
2024-01-12 19:10:20 +0100

    Play AudioUnit data rendered in callbacks
    Change-Id: I6e4b8ad466fa5656e16fbff68b1fda9b4c721443

--- src/frameworks/audio_toolbox/audio_components.rs
+++ src/frameworks/audio_toolbox/audio_components.rs
@@ -1,5 +1,9 @@
 use std::collections::HashMap;
+use std::time::Instant;
 
+use touchHLE_openal_soft_wrapper::al_types::ALuint;
+
+use crate::abi::GuestFunction;
 use crate::dyld::FunctionExports;
 use crate::environment::Environment;
 use crate::export_c_func;
@@ -32,6 +36,8 @@ pub struct AudioComponentInstanceHostObject {
     pub global_stream_format: AudioStreamBasicDescription,
     pub output_stream_format: Option<AudioStreamBasicDescription>,
     pub render_callback: Option<AURenderCallbackStruct>,
+    pub last_render_time: Option<Instant>,
+    pub al_source: Option<ALuint>,
 }
 impl Default for AudioComponentInstanceHostObject {
     fn default() -> Self {
@@ -53,6 +59,8 @@ impl Default for AudioComponentInstanceHostObject {
             },
             output_stream_format: None,
             render_callback: None,
+            last_render_time: None,
+            al_source: None,
         }
     }
 }
@@ -60,7 +68,7 @@ impl Default for AudioComponentInstanceHostObject {
 #[derive(Copy, Clone)]
 #[repr(C, packed)]
 pub struct AURenderCallbackStruct {
-    pub inputProc: ConstVoidPtr,
+    pub inputProc: AURenderCallback,
     pub inputProcRefCon: ConstVoidPtr,
 }
 unsafe impl SafeRead for AURenderCallbackStruct {}
@@ -82,6 +90,8 @@ struct OpaqueAudioComponent {}
 unsafe impl SafeRead for OpaqueAudioComponent {}
 
 type AudioComponent = MutPtr<OpaqueAudioComponent>;
+
+pub type AURenderCallback = GuestFunction;
 
 #[repr(C, packed)]
 pub struct OpaqueAudioComponentInstance {

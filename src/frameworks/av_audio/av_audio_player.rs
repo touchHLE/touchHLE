e06253c7bd74baf507b126e383fdb062c78963e1
alborrajo <alvarolopezborr@gmail.com>
2024-02-02 01:01:39 +0100

    Stub AVAudioPlayer currentTime
    Change-Id: Id33c951e3a55c7504002f79238804cc6e66c6575

--- src/frameworks/av_audio/av_audio_player.rs
+++ src/frameworks/av_audio/av_audio_player.rs
@@ -20,7 +20,7 @@ use crate::frameworks::audio_toolbox::audio_queue::{
 use crate::frameworks::carbon_core::eofErr;
 use crate::frameworks::core_audio_types::AudioStreamBasicDescription;
 use crate::frameworks::core_foundation::cf_run_loop::kCFRunLoopCommonModes;
-use crate::frameworks::foundation::{ns_string, NSInteger};
+use crate::frameworks::foundation::{ns_string, NSInteger, NSTimeInterval};
 use crate::mem::{guest_size_of, GuestUSize, MutPtr, MutVoidPtr, Ptr};
 use crate::msg;
 use crate::objc::{id, nil, release, retain, Class, ClassExports, HostObject, NSZonePtr};
@@ -237,6 +237,15 @@ pub const CLASSES: ClassExports = objc_classes! {
     let url = env.objc.borrow_mut::<AVAudioPlayerHostObject>(this).audio_file_url;
     release(env, url);
     env.objc.dealloc_object(this, &mut env.mem)
+}
+
+- (NSTimeInterval)currentTime {
+    let result = 0.0;
+    log!("TODO: [(AVAudioPlayer *) {:?} currentTime] -> {:?}", this, result);
+    result
+}
+- (())setCurrentTime:(NSTimeInterval)currentTime {
+    log!("TODO: [(AVAudioPlayer *) {:?} setCurrentTime: {}]", this, currentTime);
 }
 
 @end

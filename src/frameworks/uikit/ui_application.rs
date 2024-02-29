46794453261b233463025c15067ed431331c6007
alborrajo <alvarolopezborr@gmail.com>
2024-02-29 19:09:49 +0100

    UIResponder touch event propagation
    Change-Id: I32ce8c8ac55f46f8ec86e106b0162aef2026ecb9

--- src/frameworks/uikit/ui_application.rs
+++ src/frameworks/uikit/ui_application.rs
@@ -159,6 +159,16 @@ pub const CLASSES: ClassExports = objc_classes! {
     autorelease(env, windows)
 }
 
+// UIResponder implementation
+// From the Apple UIView docs regarding [UIResponder nextResponder]:
+// "The shared UIApplication object normally returns nil, but it returns its
+//  app delegate if that object is a subclass of UIResponder and hasn’t
+//  already been called to handle the event."
+// TODO: The latter part
+- (id)nextResponder {
+    nil
+}
+
 @end
 
 };

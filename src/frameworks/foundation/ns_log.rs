a923a13a1dd57d4d739c3f79a4f47655eac5f76e
ciciplusplus <ciciplusplus@gmail.com>
2024-03-22 21:16:59 +0100

    Implement NSLogv
    Change-Id: Ic4151d9f976b3522b7891e07e4e95355dfe1613a

--- src/frameworks/foundation/ns_log.rs
+++ src/frameworks/foundation/ns_log.rs
@@ -1,7 +1,7 @@
 //! `NSLog()`, `NSLogv()`
 
 use super::ns_string;
-use crate::abi::DotDotDot;
+use crate::abi::{DotDotDot, VaList};
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::libc::stdio::printf::printf_inner;
 use crate::objc::id;
@@ -11,6 +11,14 @@ fn NSLog(
     env: &mut Environment,
     format: id, // NSString
     args: DotDotDot,
+) {
+    NSLogv(env, format, args.start());
+}
+
+fn NSLogv(
+    env: &mut Environment,
+    format: id, // NSString
+    arg: VaList
 ) {
     // TODO: avoid copy
     let format_string = ns_string::to_rust_string(env, format);
@@ -26,7 +34,7 @@ fn NSLog(
                 format_string.as_bytes()[idx as usize]
             }
         },
-        args.start(),
+        arg,
     );
     // TODO: Should we include a timestamp, like the real NSLog?
     echo!(
@@ -37,4 +45,7 @@ fn NSLog(
     );
 }
 
-pub const FUNCTIONS: FunctionExports = &[export_c_func!(NSLog(_, _))];
+pub const FUNCTIONS: FunctionExports = &[
+    export_c_func!(NSLog(_, _)),
+    export_c_func!(NSLogv(_, _))
+];

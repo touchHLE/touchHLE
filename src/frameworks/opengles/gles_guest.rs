8fd0518385ec01a01033e10fe06163a9bebbba86
hikari_no_yume <hikari@noyu.me>
2024-04-05 17:41:51 +0200

    Revert "Add missing glGetPointerv() implementation"
    This reverts commit 289f7e7a961243b9fe812c528b52c644edf8b7c1.
    
    There is no conversion of host to guest pointers performed, so this
    will overwrite more memory than it should (64 bits instead of 32
    bits) and the returned value may be functionally useless depending on
    whether the pointer is an offset or a true guest pointer.

--- src/frameworks/opengles/gles_guest.rs
+++ src/frameworks/opengles/gles_guest.rs
@@ -129,12 +129,6 @@ fn glGetTexEnviv(env: &mut Environment, target: GLenum, pname: GLenum, params: M
         unsafe { gles.GetTexEnviv(target, pname, params) };
     });
 }
-fn glGetPointerv(env: &mut Environment, pname: GLenum, params: MutPtr<ConstPtr<GLvoid>>) {
-    with_ctx_and_mem(env, |gles, mem| {
-        let params = mem.ptr_at_mut(params, 1);
-        unsafe { gles.GetPointerv(pname, params.cast()) };
-    })
-}
 fn glHint(env: &mut Environment, target: GLenum, mode: GLenum) {
     with_ctx_and_mem(env, |gles, _mem| unsafe { gles.Hint(target, mode) })
 }
@@ -1063,7 +1057,6 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(glGetFloatv(_, _)),
     export_c_func!(glGetIntegerv(_, _)),
     export_c_func!(glGetTexEnviv(_, _, _)),
-    export_c_func!(glGetPointerv(_, _)),
     export_c_func!(glHint(_, _)),
     export_c_func!(glFlush()),
     export_c_func!(glGetString(_)),

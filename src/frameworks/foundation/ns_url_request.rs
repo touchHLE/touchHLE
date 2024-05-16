a7516d97d628803a939f98fe5b1ff0b2e320eabe
ciciplusplus-win <ciciplusplus@gmail.com>
2024-05-16 12:06:48 +0300

    NSURLRequest and NSMutableURLRequest stubs
    Co-authored-by: Lyra <teromene@teromene.fr>
    Change-Id: Ia5b528a12417a6db93df248b78d0af6d5ec36288

---
+++ src/frameworks/foundation/ns_url_request.rs
@@ -0,0 +1,46 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+//! `NSURLRequest and NSMutableURLRequest`.
+
+use super::{NSTimeInterval, NSUInteger};
+use crate::frameworks::foundation::ns_string::to_rust_string;
+use crate::msg;
+use crate::objc::{id, nil, objc_classes, ClassExports, HostObject};
+
+struct NSURLRequestHostObject {}
+impl HostObject for NSURLRequestHostObject {}
+
+type NSURLRequestCachePolicy = NSUInteger;
+
+pub const CLASSES: ClassExports = objc_classes! {
+
+(env, this, _cmd);
+
+@implementation NSURLRequest: NSObject
+
++ (id)requestWithURL:(id)url
+         cachePolicy:(NSURLRequestCachePolicy)cache_policy
+     timeoutInterval:(NSTimeInterval)timeout_interval {
+    if url == nil {
+        return nil;
+    }
+    let url_str: id = msg![env; url path];
+    log!(
+        "TODO: [NSURLRequest requestWithURL:{} cachePolicy:{} timeoutInterval:{}]",
+        to_rust_string(env, url_str),
+        cache_policy,
+        timeout_interval,
+    );
+    nil
+}
+
+@end
+
+@implementation NSMutableURLRequest: NSURLRequest
+//TODO
+@end
+
+};

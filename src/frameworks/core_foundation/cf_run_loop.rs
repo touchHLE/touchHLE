69dbac332e6b8881f57de07f5b63468c779990ca
ciciplusplus <ciciplusplus@gmail.com>
2024-06-30 22:13:22 +0200

    Stubish CFRunLoopRunInMode() implementation
    Change-Id: I30edfd746e2a7e9e9026b90a4c4c7bd15b097d85

--- src/frameworks/core_foundation/cf_run_loop.rs
+++ src/frameworks/core_foundation/cf_run_loop.rs
@@ -9,7 +9,10 @@
 //! but here it is the same type.
 
 use crate::dyld::{export_c_func, ConstantExports, FunctionExports, HostConstant};
-use crate::objc::msg_class;
+use crate::frameworks::core_foundation::time::CFTimeInterval;
+use crate::frameworks::foundation::ns_run_loop::run_run_loop_single_iteration;
+use crate::frameworks::foundation::ns_string;
+use crate::objc::{msg, msg_class};
 use crate::Environment;
 
 pub type CFRunLoopRef = super::CFTypeRef;
@@ -21,6 +24,30 @@ fn CFRunLoopGetCurrent(env: &mut Environment) -> CFRunLoopRef {
 
 pub fn CFRunLoopGetMain(env: &mut Environment) -> CFRunLoopRef {
     msg_class![env; NSRunLoop mainRunLoop]
+}
+
+fn CFRunLoopRunInMode(
+    env: &mut Environment,
+    mode: CFRunLoopMode,
+    seconds: CFTimeInterval,
+    _return_something: bool,
+) -> i32 {
+    let default_mode = ns_string::get_static_str(env, kCFRunLoopDefaultMode);
+    let common_modes = ns_string::get_static_str(env, kCFRunLoopCommonModes);
+    // TODO: handle other modes
+    assert!(
+        msg![env; mode isEqualToString:default_mode]
+            || msg![env; mode isEqualToString:common_modes]
+    );
+    assert!(seconds <= 0.001);
+    // TODO: we're currently supporting only the main run loop
+    log_dbg!(
+        "TODO: properly implement CFRunLoopRunInMode [current thread {}], running a single iteration of the main run loop",
+        env.current_thread
+    );
+    let main_run_loop = CFRunLoopGetMain(env);
+    run_run_loop_single_iteration(env, main_run_loop);
+    1 // kCFRunLoopRunFinished
 }
 
 pub const kCFRunLoopCommonModes: &str = "kCFRunLoopCommonModes";
@@ -40,4 +67,5 @@ pub const CONSTANTS: ConstantExports = &[
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CFRunLoopGetCurrent()),
     export_c_func!(CFRunLoopGetMain()),
+    export_c_func!(CFRunLoopRunInMode(_, _, _)),
 ];

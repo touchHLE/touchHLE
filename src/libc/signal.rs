04e28fa73461620316675eb04ec17b6d26ae91fa
ciciplusplus <ciciplusplus@gmail.com>
2024-08-21 14:55:34 +0300

    errno refactoring
    We clean now errno value to 0 before calling any libc function which
    may set it. It is still **not** POSIX complaint, but is better than
    always returning 0 as we can start implement real errno support for
    some of the functions and, eventually, have a proper support.
    
    Change-Id: I7c5c877c9b8f0719449293898db07c946d4888eb

--- src/libc/signal.rs
+++ src/libc/signal.rs
@@ -7,14 +7,21 @@
 use crate::dyld::FunctionExports;
 use crate::environment::Environment;
 use crate::export_c_func;
+use crate::libc::errno::set_errno;
 use crate::mem::{ConstVoidPtr, MutVoidPtr, Ptr};
 
-fn sigaction(_env: &mut Environment, signum: i32, act: ConstVoidPtr, oldact: MutVoidPtr) -> i32 {
+fn sigaction(env: &mut Environment, signum: i32, act: ConstVoidPtr, oldact: MutVoidPtr) -> i32 {
+    // TODO: handle errno properly
+    set_errno(env, 0);
+
     log!("TODO: sigaction({:?}, {:?}, {:?})", signum, act, oldact);
     0
 }
 
-fn signal(_env: &mut Environment, signum: i32, handler: MutVoidPtr) -> MutVoidPtr {
+fn signal(env: &mut Environment, signum: i32, handler: MutVoidPtr) -> MutVoidPtr {
+    // TODO: handle errno properly
+    set_errno(env, 0);
+
     log!("TODO: signal({:?}, {:?})", signum, handler);
     Ptr::null()
 }

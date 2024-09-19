008dd0d314aadab285941ce44bb6b69826dfe71c
ciciplusplus <ciciplusplus@gmail.com>
2024-09-20 00:02:14 +0200

    Refactor NSUserDefaults and implement most of their methods
    Change-Id: Iaec1d2b501bfd5f12a8d076cc12b312b6b2cc776

--- src/frameworks/foundation/ns_user_defaults.rs
+++ src/frameworks/foundation/ns_user_defaults.rs
@@ -8,14 +8,16 @@
 //! References:
 //! - Apple's [Preferences and Settings Programming Guide](https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/UserDefaults/AboutPreferenceDomains/AboutPreferenceDomains.html).
 
-use super::ns_dictionary::dict_from_keys_and_objects;
-use super::ns_string;
-use crate::objc::{id, msg_class, objc_classes, ClassExports};
+use super::{ns_string, NSInteger};
+use crate::objc::{
+    autorelease, id, msg, msg_class, nil, objc_classes, release, retain, ClassExports, HostObject,
+    NSZonePtr,
+};
 use crate::Environment;
 
 #[derive(Default)]
 pub struct State {
-    /// `NSDictionary*`
+    /// `NSUserDefaults*`
     standard_defaults: Option<id>,
 }
 impl State {
@@ -23,6 +25,12 @@ impl State {
         &mut env.framework_state.foundation.ns_user_defaults
     }
 }
+
+struct NSUserDefaultsHostObject {
+    // NSMutableDictionary *
+    dictionary: id,
+}
+impl HostObject for NSUserDefaultsHostObject {}
 
 pub const CLASSES: ClassExports = objc_classes! {
 
@@ -34,16 +42,97 @@ pub const CLASSES: ClassExports = objc_classes! {
     if let Some(existing) = State::get(env).standard_defaults {
         existing
     } else {
-        // TODO: Are there other default keys we need to set?
-        let langs_value: id = msg_class![env; NSLocale preferredLanguages];
-        let langs_key: id = ns_string::get_static_str(env, "AppleLanguages");
-        let new = dict_from_keys_and_objects(env, &[(langs_key, langs_value)]);
-        State::get(env).standard_defaults = Some(new);
-        new
+        let defaults = msg![env; this alloc];
+        let defaults = msg![env; defaults init];
+        State::get(env).standard_defaults = Some(defaults);
+        defaults
     }
 }
 
++ (id)allocWithZone:(NSZonePtr)_zone {
+    let host_object = Box::new(NSUserDefaultsHostObject {
+        dictionary: nil,
+    });
+    env.objc.alloc_object(this, host_object, &mut env.mem)
+}
+
 // TODO: plist methods etc
+
+- (id)init {
+    // TODO: Are there other default keys we need to set?
+    let langs_value: id = msg_class![env; NSLocale preferredLanguages];
+    let langs_key: id = ns_string::get_static_str(env, "AppleLanguages");
+
+    let dict = msg_class![env; NSMutableDictionary dictionary];
+    () = msg![env; dict setObject:langs_value forKey:langs_key];
+
+    retain(env, dict);
+    env.objc.borrow_mut::<NSUserDefaultsHostObject>(this).dictionary = dict;
+    this
+}
+
+- (())dealloc {
+    let dict = env.objc.borrow::<NSUserDefaultsHostObject>(this).dictionary;
+    release(env, dict);
+
+    env.objc.dealloc_object(this, &mut env.mem);
+}
+
+- (id)dictionaryRepresentation { // NSDictionary *
+    let dict = env.objc.borrow::<NSUserDefaultsHostObject>(this).dictionary;
+    let dict = msg![env; dict copy];
+    autorelease(env, dict)
+}
+
+- (id)objectForKey:(id)key { // NSString*
+    let dict = env.objc.borrow::<NSUserDefaultsHostObject>(this).dictionary;
+    msg![env; dict objectForKey:key]
+}
+- (())setObject:(id)object
+         forKey:(id)key { // NSString*
+    let dict = env.objc.borrow::<NSUserDefaultsHostObject>(this).dictionary;
+    msg![env; dict setObject:object forKey:key]
+}
+- (())removeObjectForKey:(id)key {
+    let dict = env.objc.borrow::<NSUserDefaultsHostObject>(this).dictionary;
+    msg![env; dict removeObjectForKey:key]
+}
+
+- (bool)boolForKey:(id)key { // NSString *
+    let val: id = msg![env; this objectForKey:key];
+    msg![env; val boolValue]
+}
+- (())setBool:(bool)value
+       forKey:(id)key { // NSString *
+    let num: id = msg_class![env; NSNumber numberWithBool:value];
+    msg![env; this setObject:num forKey:key]
+}
+
+- (NSInteger)integerForKey:(id)key {
+    let val: id = msg![env; this objectForKey:key];
+    msg![env; val integerValue]
+}
+- (())setInteger:(NSInteger)value
+          forKey:(id)key {
+    let num: id = msg_class![env; NSNumber numberWithInteger:value];
+    msg![env; this setObject:num forKey:key]
+}
+
+- (id)stringForKey:(id)key {
+    let val: id = msg![env; this objectForKey:key];
+    if val == nil {
+        return nil;
+    }
+    let ns_string_class = env.objc.get_known_class("NSString", &mut env.mem);
+    if env.objc.class_is_subclass_of(val, ns_string_class) {
+        return val;
+    }
+    let ns_number_class = env.objc.get_known_class("NSNumber", &mut env.mem);
+    if env.objc.class_is_subclass_of(val, ns_number_class) {
+        todo!();
+    }
+    nil
+}
 
 @end
 

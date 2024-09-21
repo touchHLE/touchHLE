587b5d320e868239665db6b717fc73d78e6dcbee
ciciplusplus <ciciplusplus@gmail.com>
2024-09-21 23:34:36 +0200

    NSPropertyListSerialization dataFromPropertyList:format:errorDescription: method
    Change-Id: I71071fc446218b16d5fe4f2bfda18342e5a21a72

--- src/frameworks/foundation/ns_property_list_serialization.rs
+++ src/frameworks/foundation/ns_property_list_serialization.rs
@@ -5,13 +5,63 @@
  */
 //! `NSPropertyListSerialization`.
 
-use super::{ns_array, ns_dictionary, ns_string, NSUInteger};
+use super::{ns_array, ns_data, ns_dictionary, ns_string, NSUInteger};
+use super::{
+    ns_array::ArrayHostObject, ns_dictionary::DictionaryHostObject, ns_value::NSNumberHostObject,
+};
 use crate::fs::GuestPath;
 use crate::mem::MutPtr;
-use crate::objc::{id, msg, msg_class, nil, release};
+use crate::objc::{id, msg, msg_class, nil, objc_classes, release, Class, ClassExports};
 use crate::Environment;
 use plist::Value;
 use std::io::Cursor;
+
+pub type NSPropertyListMutabilityOptions = NSUInteger;
+pub const NSPropertyListImmutable: NSPropertyListMutabilityOptions = 0;
+
+pub type NSPropertyListFormat = NSUInteger;
+pub const NSPropertyListBinaryFormat_v1_0: NSPropertyListFormat = 200;
+
+pub const CLASSES: ClassExports = objc_classes! {
+
+(env, this, _cmd);
+
+@implementation NSPropertyListSerialization: NSObject
+
++ (id)dataFromPropertyList:(id)plist
+                    format:(NSPropertyListFormat)format
+                errorDescription:(MutPtr<id>)error_string { // NSString **
+    assert_eq!(format, NSPropertyListBinaryFormat_v1_0); // TODO
+    assert!(error_string.is_null()); // TODO
+
+    let value = serialize_plist(env, plist);
+    log_dbg!("dataFromPropertyList value {:?}", value);
+    let mut buf = Vec::new();
+    value.to_writer_binary(&mut buf).unwrap();
+    let len: u32 = buf.len().try_into().unwrap();
+    log_dbg!("dataFromPropertyList buf len {}", len);
+    let ptr = env.mem.alloc(len);
+    env.mem.bytes_at_mut(ptr.cast(), len).copy_from_slice(&buf[..]);
+    msg_class![env; NSData dataWithBytesNoCopy:ptr length:len]
+}
+
++ (id)propertyListFromData:(id)data // NSData *
+          mutabilityOption:(NSPropertyListMutabilityOptions)opt
+                    format:(MutPtr<NSPropertyListFormat>)format
+          errorDescription:(MutPtr<id>)error_string { // NSString **
+    assert_eq!(opt, NSPropertyListImmutable); // TODO
+    assert!(format.is_null()); // TODO
+    assert!(error_string.is_null()); // TODO
+
+    let slice = ns_data::to_rust_slice(env, data);
+    let root = Value::from_reader(Cursor::new(slice)).unwrap();
+    assert!(root.as_array().is_some() || root.as_dictionary().is_some());
+    deserialize_plist(env, &root)
+}
+
+@end
+
+};
 
 /// Internals of `initWithContentsOfFile:` on `NSArray` and `NSDictionary`.
 /// Returns `nil` on failure.
@@ -114,5 +164,72 @@ fn deserialize_plist(env: &mut Environment, value: &Value) -> id {
         _ => {
             unreachable!() // enum is marked inexhaustive, but shouldn't be
         }
+    }
+}
+
+fn serialize_plist(env: &mut Environment, plist: id) -> Value {
+    let class: Class = msg![env; plist class];
+
+    let dict_class = env.objc.get_known_class("NSDictionary", &mut env.mem);
+    let arr_class = env.objc.get_known_class("NSArray", &mut env.mem);
+    let str_class = env.objc.get_known_class("NSString", &mut env.mem);
+
+    if env.objc.class_is_subclass_of(class, dict_class) {
+        // only our internal implementation is supported
+        assert!(env.objc.get_class_name(class).starts_with("_touchHLE_NS"));
+
+        let mut dict = plist::dictionary::Dictionary::new();
+        let dict_host_obj: DictionaryHostObject = std::mem::take(env.objc.borrow_mut(plist));
+        let mut key_vals = Vec::with_capacity(dict_host_obj.count as usize);
+        for collisions in dict_host_obj.map.values() {
+            for &(key, value) in collisions {
+                key_vals.push((key, value));
+            }
+        }
+        *env.objc.borrow_mut(plist) = dict_host_obj;
+        for (key, val) in key_vals {
+            let key_class: Class = msg![env; key class];
+
+            // only string keys are supported
+            assert!(env.objc.class_is_subclass_of(key_class, str_class));
+            assert!(env
+                .objc
+                .get_class_name(key_class)
+                .starts_with("_touchHLE_NS"));
+
+            let key_string = ns_string::to_rust_string(env, key);
+            let val_plist = serialize_plist(env, val);
+            dict.insert(String::from(key_string), val_plist);
+        }
+        Value::Dictionary(dict)
+    } else if env.objc.class_is_subclass_of(class, arr_class) {
+        // only our internal implementation is supported
+        assert!(env.objc.get_class_name(class).starts_with("_touchHLE_NS"));
+
+        let arr_host_obj: ArrayHostObject = std::mem::take(env.objc.borrow_mut(plist));
+        let arr: Vec<Value> = arr_host_obj
+            .array
+            .iter()
+            .map(|&value| serialize_plist(env, value))
+            .collect();
+        *env.objc.borrow_mut(plist) = arr_host_obj;
+        Value::Array(arr)
+    } else if env.objc.class_is_subclass_of(class, str_class) {
+        // only our internal implementation is supported
+        assert!(env.objc.get_class_name(class).starts_with("_touchHLE_NS"));
+
+        let s = ns_string::to_rust_string(env, plist);
+        Value::String(s.to_string())
+    } else if class == env.objc.get_known_class("NSNumber", &mut env.mem) {
+        let num = env.objc.borrow::<NSNumberHostObject>(plist);
+        match num {
+            NSNumberHostObject::Bool(b) => Value::Boolean(*b),
+            NSNumberHostObject::Int(i) => Value::from(*i),
+            NSNumberHostObject::Float(f) => Value::from(*f),
+            NSNumberHostObject::LongLong(ll) => Value::from(*ll),
+            _ => todo!("num {:?}", num),
+        }
+    } else {
+        unimplemented!("class {}", env.objc.get_class_name(class))
     }
 }

b31d59baa517c11747da9eb21c260327763c45b7
ciciplusplus <ciciplusplus@gmail.com>
2024-09-21 23:35:36 +0200

    NSUserDefaults synchronize method
    Change-Id: Ie265cc6c4172bbfc0c59f66c3f163b9f7467efaf

--- src/frameworks/foundation/ns_dictionary.rs
+++ src/frameworks/foundation/ns_dictionary.rs
@@ -356,6 +356,16 @@ pub const CLASSES: ClassExports = objc_classes! {
     *env.objc.borrow_mut(this) = host_obj;
 }
 
+- (())addEntriesFromDictionary:(id)other { // NSDictionary *
+    let host_obj: DictionaryHostObject = std::mem::take(env.objc.borrow_mut(other));
+    for (k, v) in host_obj.map.values().flatten() {
+        let existing: id = msg![env; this objectForkey:(*k)];
+        assert_eq!(existing, nil); // TODO
+        () = msg![env; this setObject:(*v) forKey:(*k)];
+    }
+    *env.objc.borrow_mut(this) = host_obj;
+}
+
 - (id)description {
     build_description(env, this)
 }

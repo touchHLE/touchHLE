db59e07c02cd2c1854a297765f68350121fa1836
ciciplusplus <ciciplusplus@gmail.com>
2024-09-21 23:35:06 +0200

    NSDictionary writeToFile:atomically: method
    Change-Id: I1a22dbda35b12e3745f9d4d34ae719caa0a71153

--- src/frameworks/foundation/ns_dictionary.rs
+++ src/frameworks/foundation/ns_dictionary.rs
@@ -5,11 +5,14 @@
  */
 //! The `NSDictionary` class cluster, including `NSMutableDictionary`.
 
-use super::ns_property_list_serialization::deserialize_plist_from_file;
+use super::ns_property_list_serialization::{
+    deserialize_plist_from_file, NSPropertyListBinaryFormat_v1_0,
+};
 use super::{ns_string, ns_url, NSUInteger};
 use crate::abi::VaList;
 use crate::frameworks::foundation::ns_string::{from_rust_string, to_rust_string};
 use crate::fs::GuestPath;
+use crate::mem::{MutPtr, Ptr};
 use crate::objc::{
     autorelease, id, msg, msg_class, nil, objc_classes, release, retain, ClassExports, HostObject,
     NSZonePtr,
@@ -29,7 +32,7 @@ pub(super) struct DictionaryHostObject {
     /// hash-map, which is not ideally efficient. :)
     /// The keys are the hash values, the values are a list of key-value pairs
     /// where the keys have the same hash value.
-    map: HashMap<Hash, Vec<(id, id)>>,
+    pub(super) map: HashMap<Hash, Vec<(id, id)>>,
     pub(super) count: NSUInteger,
 }
 impl HostObject for DictionaryHostObject {}
@@ -226,13 +229,6 @@ pub const CLASSES: ClassExports = objc_classes! {
     autorelease(env, new)
 }
 
-// NSCopying implementation
-- (id)copyWithZone:(NSZonePtr)_zone {
-    let entries: Vec<_> =
-        env.objc.borrow_mut::<DictionaryHostObject>(this).map.values().flatten().copied().collect();
-    dict_from_keys_and_objects(env, &entries)
-}
-
 @end
 
 // Our private subclass that is the single implementation of NSDictionary for
@@ -271,8 +267,32 @@ pub const CLASSES: ClassExports = objc_classes! {
     res
 }
 
+- (id)mutableCopyWithZone:(NSZonePtr)_zone {
+    let mut_dict: id = msg_class![env; NSMutableDictionary alloc];
+    let host_obj: DictionaryHostObject = std::mem::take(env.objc.borrow_mut(this));
+    for (k, v) in host_obj.map.values().flatten() {
+        () = msg![env; mut_dict setObject:(*v) forKey:(*k)];
+    }
+    *env.objc.borrow_mut(this) = host_obj;
+    mut_dict
+}
+
 - (id)description {
     build_description(env, this)
+}
+
+// FIXME: writes should be atomic
+- (bool)writeToFile:(id)path // NSString*
+         atomically:(bool)_use_aux_file {
+    let file_path = to_rust_string(env, path);
+    let error_desc: MutPtr<id> = Ptr::null();
+    let data: id = msg_class![env; NSPropertyListSerialization
+            dataFromPropertyList:this
+                          format:NSPropertyListBinaryFormat_v1_0
+                errorDescription:error_desc];
+    let res = msg![env; data writeToFile:path atomically:true];
+    log_dbg!("[(NSDictionary *){:?} writeToFile:{:?} atomically:_] -> {}", this, file_path, res);
+    res
 }
 
 @end
@@ -316,6 +336,13 @@ pub const CLASSES: ClassExports = objc_classes! {
     let res = host_obj.lookup(env, key);
     *env.objc.borrow_mut(this) = host_obj;
     res
+}
+
+// NSCopying implementation
+- (id)copyWithZone:(NSZonePtr)_zone {
+    let entries: Vec<_> =
+        env.objc.borrow_mut::<DictionaryHostObject>(this).map.values().flatten().copied().collect();
+    dict_from_keys_and_objects(env, &entries)
 }
 
 - (())setObject:(id)object

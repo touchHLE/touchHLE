fb4c27cef26e2bf6c666d88dcdb45c81898fa1eb
alborrajo <alvarolopezborr@gmail.com>
2024-09-25 22:13:39 +0200

    UILabel stubs
    Change-Id: Icb2eab44ae2988409ce2ceb3e76fb6a999dc55cc

--- src/frameworks/uikit/ui_view/ui_label.rs
+++ src/frameworks/uikit/ui_view/ui_label.rs
@@ -20,6 +20,8 @@ use crate::objc::{
     retain, ClassExports, NSZonePtr,
 };
 
+type UIBaselineAdjustment = NSInteger;
+
 pub struct UILabelHostObject {
     superclass: super::UIViewHostObject,
     /// `NSString*`
@@ -187,6 +189,15 @@ pub const CLASSES: ClassExports = objc_classes! {
     };
     msg_super![env; this setBackgroundColor:color]
 }
+
+- (())setShadowColor:(id)color { // UIColor*
+    log!("TODO: [(UILabel*) {:?} setShadowColor:{:?}]", this, color);
+}
+
+- (())setShadowOffset:(CGSize)value {
+    log!("TODO: [(UILabel*) {:?} setShadowOffset:{:?}]", this, value);
+}
+
 - (())setOpaque:(bool)_opaque {
     // Built-in views don't have user-controlled opaqueness.
 }
@@ -216,6 +227,18 @@ pub const CLASSES: ClassExports = objc_classes! {
         log!("TODO: UILabel numberOfLines > 1 (label {:?})", this);
     }
     () = msg![env; this setNeedsDisplay];
+}
+
+- (())setAdjustsFontSizeToFitWidth:(bool)value {
+    log!("TODO: [(UILabel*) {:?} setAdjustsFontSizeToFitWidth:{}]", this, value);
+}
+
+- (())setBaselineAdjustment:(UIBaselineAdjustment)value {
+    log!("TODO: [(UILabel*) {:?} setBaselineAdjustment:{}]", this, value);
+}
+
+- (())sizeToFit {
+    log!("TODO: [UILabel*) {:?} sizeToFit]", this);
 }
 
 - (())drawRect:(CGRect)_rect {

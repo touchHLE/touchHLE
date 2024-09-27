af7e9d0f06ed421f8a0298cf48aec24810bf3164
alborrajo <alvarolopezborr@gmail.com>
2024-09-28 00:49:26 +0200

    Implementation of [NSMutableArray countByEnumeratingWithState:objects:count:] for the default implementing class
    Change-Id: Id10427229dd5755064ccdcd9b47fd8e9c00c4ca0

--- src/frameworks/foundation/ns_array.rs
+++ src/frameworks/foundation/ns_array.rs
@@ -368,6 +368,22 @@ pub const CLASSES: ClassExports = objc_classes! {
     object_enumerator_inner(env, this)
 }
 
+// NSFastEnumeration implementation
+- (NSUInteger)countByEnumeratingWithState:(MutPtr<NSFastEnumerationState>)state
+                                  objects:(MutPtr<id>)stackbuf
+                                    count:(NSUInteger)len {
+    let mut iterator = env.objc.borrow_mut::<ArrayHostObject>(this).array.iter().copied();
+    fast_enumeration_helper(&mut env.mem, this, &mut iterator, state, stackbuf, len)
+}
+
+
+// TODO: init methods etc
+
+- (id)initWithCapacity:(NSUInteger)numItems {
+    env.objc.borrow_mut::<ArrayHostObject>(this).array.reserve(numItems as usize);
+    this
+}
+
 - (NSUInteger)count {
     env.objc.borrow::<ArrayHostObject>(this).array.len().try_into().unwrap()
 }

7bb6bc729d1f27b977e9d3fe4a86203c7216d311
Keyoke <591485+hujerhoe@users.noreply.github.com>
2024-10-25 00:51:20 +0200

    Implement dummy UISwitch class
    Change-Id: I092bc0a56c7d7163a9bcf61adf2a2b090959b6fa

---
+++ src/frameworks/uikit/ui_view/ui_control/ui_switch.rs
@@ -0,0 +1,24 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+//! `UISwitch`.
+
+use crate::objc::{impl_HostObject_with_superclass, objc_classes, ClassExports};
+
+#[derive(Default)]
+pub struct UISwitchHostObject {
+    superclass: super::UIControlHostObject,
+}
+impl_HostObject_with_superclass!(UISwitchHostObject);
+
+pub const CLASSES: ClassExports = objc_classes! {
+
+(env, this, _cmd);
+
+@implementation UISwitch: UIControl
+
+@end
+
+};

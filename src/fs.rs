cac8ec16535c74e3c6ba4eae66431543ff6f3ce4
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-02-27 22:41:34 -0700

    Add zlib dylib
    Change-Id: I2db5f219d43d2f4f875b81cab51674895a199cb8

--- src/fs.rs
+++ src/fs.rs
@@ -552,6 +552,10 @@ impl Fs {
             .with_child(
                 "libstdc++.6.0.9.dylib",
                 FsNode::resource_file(format!("{}/libstdc++.6.0.9.dylib", DYLIBS_DIR)),
+            )
+            .with_child(
+                "libz.1.dylib",
+                FsNode::resource_file(format!("{}/libz.1.dylib", DYLIBS_DIR)),
             );
 
         let mut app_dir_children = HashMap::new();

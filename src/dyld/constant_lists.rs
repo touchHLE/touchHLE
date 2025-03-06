b5f26d54791246dee70cc9bdf65808ebbe88380e
apexad <1437332+apexad@users.noreply.github.com>
2025-03-05 21:07:15 -0700

    Add CATransaction stub
    Change-Id: Ice8b9fc5aaf217416dacae057c08448295da7221

--- src/dyld/constant_lists.rs
+++ src/dyld/constant_lists.rs
@@ -20,6 +20,7 @@ pub const CONSTANT_LISTS: &[super::ConstantExports] = &[
     core_animation::ca_animation::CONSTANTS,
     core_animation::ca_layer::CONSTANTS,
     core_animation::ca_media_timing_function::CONSTANTS,
+    core_animation::ca_transaction::CONSTANTS,
     core_foundation::cf_allocator::CONSTANTS,
     core_foundation::cf_boolean::CONSTANTS,
     core_foundation::cf_bundle::CONSTANTS,

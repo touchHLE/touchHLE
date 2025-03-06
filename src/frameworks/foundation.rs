312eb041c176189874eefddf846b3d71f1623444
apexad <1437332+apexad@users.noreply.github.com>
2025-03-05 21:20:28 -0700

    Implement NSProccessInfo as singleton, add physicalMemory and processName
    Change-Id: I2668222a52c0c95b6427ef5bd7d3986202d4c92b

--- src/frameworks/foundation.rs
+++ src/frameworks/foundation.rs
@@ -62,6 +62,7 @@ pub struct State {
     ns_locale: ns_locale::State,
     ns_notification_center: ns_notification_center::State,
     ns_null: ns_null::State,
+    ns_process_info: ns_process_info::State,
     ns_run_loop: ns_run_loop::State,
     ns_string: ns_string::State,
     ns_thread: ns_thread::State,

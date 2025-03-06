ff2d66e5baf6ba41288b436fcc05e2f5c4d2dfb4
apexad <1437332+apexad@users.noreply.github.com>
2025-03-05 21:16:07 -0700

    Add CGBlendMode Stub and a few functions to cg_context
    Change-Id: If8bb27eb87a99e07d80627d1120c4279d6e20ada

--- src/frameworks/core_graphics.rs
+++ src/frameworks/core_graphics.rs
@@ -19,4 +19,36 @@ pub mod cg_image;
 
 pub type CGFloat = f32;
 
+pub type CGBlendMode = i32;
+/* TODO: Make CGBlendMode type safe using enum:
+   normal = 0,
+   multiply = 1,
+   screen = 2,
+   overlay = 3,
+   darken = 4,
+   lighten = 5,
+   colorDodge = 6,
+   colorBurn = 7,
+   softLight = 8,
+   hardLight = 9,
+   difference = 10,
+   exclusion = 11,
+   hue = 12,
+   saturation = 13,
+   color = 14,
+   luminosity = 15,
+   clear = 16,
+   copy = 17,
+   sourceIn = 18,
+   sourceOut = 19,
+   sourceAtop = 20,
+   destinationOver = 21,
+   destinationIn = 22,
+   destinationOut = 23,
+   destinationAtop = 24,
+   xor = 25,
+   plusDarker = 26,
+   plusLighter = 27,
+*/
+
 pub use cg_geometry::{CGPoint, CGRect, CGSize};

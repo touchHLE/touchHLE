6b688bb5b208bae842b3e92242e64366678ddfe4
apexad <1437332+apexad@users.noreply.github.com>
2025-03-05 21:20:28 -0700

    Add proccessInfo and physicalMemory functions to NSProcessInfo
    Change-Id: I2668222a52c0c95b6427ef5bd7d3986202d4c92b

--- src/frameworks/foundation/ns_process_info.rs
+++ src/frameworks/foundation/ns_process_info.rs
@@ -5,18 +5,48 @@
  */
 //! `NSProcessInfo`.
 
-use super::NSTimeInterval;
-use crate::objc::{objc_classes, ClassExports};
+use super::{NSTimeInterval, NSUInteger};
+use crate::objc::{id, objc_classes, ClassExports};
 use std::time::Instant;
 
+// using 2GB; see mocked memory data src/libc/mach_host.rs
+pub const PHYSICAL_MEMORY: u32 = 2 * 1024 * 1024 * 1024;
 pub const CLASSES: ClassExports = objc_classes! {
 
 (env, this, _cmd);
 
 @implementation NSProcessInfo: NSObject
 
+// Class method returning the shared singleton instance.
+// Equivalent to +[NSProcessInfo processInfo].
++ (id)processInfo {
+    this.cast()
+}
+
+// systemUptime is typically an *instance* method returning uptime
+// since boot in seconds. However, to match real-world app usage,
+// we also expose it as a class method.
+// This handles cases where apps store the class object
+// instead of the singleton and then call systemUptime on it.
 + (NSTimeInterval)systemUptime {
     Instant::now().duration_since(env.startup_time).as_secs_f64()
+}
+
+// physicalMemory is typically an *instance* method
+// However, it has the same issue as sytemUptime
+// So, we must provide it as a class method too
++ (NSUInteger)physicalMemory {
+    PHYSICAL_MEMORY
+}
+
+// Correct, instance version of systemUptime.
+- (NSTimeInterval)systemUptime {
+    Instant::now().duration_since(env.startup_time).as_secs_f64()
+}
+
+// Correct, instance verson of physicalMemory
+- (NSUInteger)physicalMemory {
+    PHYSICAL_MEMORY
 }
 
 @end

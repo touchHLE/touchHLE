16ed46bceb74eba82c613d6c4171cdf5313a81dd
apexad <1437332+apexad@users.noreply.github.com>
2025-03-05 21:11:07 -0700

    Add UIBarButtonItem
    Change-Id: I98e80489e3e756b024171cfafaf7ab54729ac504

---
+++ src/frameworks/uikit/ui_view/ui_control/ui_bar_button_item.rs
@@ -0,0 +1,164 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+//! `UIBarButtonItem`.
+
+use crate::frameworks::foundation::ns_string::to_rust_string;
+use crate::frameworks::uikit::ui_view::ui_control::UIControlEventTouchUpInside;
+use crate::objc::{
+    id, impl_HostObject_with_superclass, msg, msg_super, nil, objc_classes, ClassExports,
+    NSZonePtr, SEL,
+};
+
+struct UIBarButtonItemHostObject {
+    superclass: super::UIControlHostObject,
+    title: id,
+    style: id,
+    target: id,
+    action: Option<SEL>,
+    system_item: i32,
+}
+
+impl_HostObject_with_superclass!(UIBarButtonItemHostObject);
+
+impl Default for UIBarButtonItemHostObject {
+    fn default() -> Self {
+        Self {
+            superclass: Default::default(),
+            title: nil,
+            style: nil,
+            target: nil,
+            action: None,
+            system_item: 0,
+        }
+    }
+}
+
+pub const CLASSES: ClassExports = objc_classes! {
+
+    (env, this, _cmd);
+
+    @implementation UIBarButtonItem: UIControl
+
+    + (id)allocWithZone:(NSZonePtr)_zone {
+        let host_object = Box::<UIBarButtonItemHostObject>::default();
+        env.objc.alloc_object(this, host_object, &mut env.mem)
+    }
+
+    - (id)init {
+        msg_super![env; this init]
+    }
+
+    - (id)initWithTitle:(id)title
+                  style:(id)style
+                 target:(id)target
+                 action:(SEL)action
+    {
+        log_dbg!(
+            "[(UIBarButtonItem*){:?} initWithTitle:{:?} style:{:?} target:{:?} action:{:?}]",
+            this,
+            to_rust_string(env, title),
+            style,
+            target,
+            action
+        );
+
+        let this: id = msg_super![env; this init];
+
+        let host = env.objc.borrow_mut::<UIBarButtonItemHostObject>(this);
+        host.title = title;
+        host.style = style;
+        host.target = target;
+        host.action = Some(action);
+        host.system_item = 0;
+
+        if target != nil {
+            () = msg![env; this addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+        }
+
+        this
+    }
+
+    - (id)initWithBarButtonSystemItem:(i32)system_item
+                               target:(id)target
+                               action:(SEL)action
+    {
+        log_dbg!(
+            "[(UIBarButtonItem*){:?} initWithBarButtonSystemItem:{} target:{:?} action:{:?}]",
+            this,
+            system_item,
+            target,
+            action
+        );
+
+        let this: id = msg_super![env; this init];
+
+        let host = env.objc.borrow_mut::<UIBarButtonItemHostObject>(this);
+        host.system_item = system_item;
+        host.target = target;
+        host.action = Some(action);
+
+        if target != nil {
+            () = msg![env; this addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+        }
+
+        this
+    }
+
+    - (id)title {
+        env.objc.borrow::<UIBarButtonItemHostObject>(this).title
+    }
+    - (())setTitle:(id)title {
+        env.objc.borrow_mut::<UIBarButtonItemHostObject>(this).title = title;
+    }
+
+    - (id)style {
+        env.objc.borrow::<UIBarButtonItemHostObject>(this).style
+    }
+    - (())setStyle:(id)style {
+        env.objc.borrow_mut::<UIBarButtonItemHostObject>(this).style = style;
+    }
+
+    - (id)target {
+        env.objc.borrow::<UIBarButtonItemHostObject>(this).target
+    }
+    - (())setTarget:(id)target {
+        env.objc.borrow_mut::<UIBarButtonItemHostObject>(this).target = target;
+    }
+
+    - (SEL)action {
+        match env.objc.borrow::<UIBarButtonItemHostObject>(this).action {
+            Some(sel) => sel,
+            None => {
+                log!("Warning: UIBarButtonItem has no action set!");
+                env.objc.lookup_selector("undefinedSelector").unwrap()
+            }
+        }
+    }
+
+    - (())setAction:(SEL)action {
+        env.objc.borrow_mut::<UIBarButtonItemHostObject>(this).action = Some(action);
+    }
+
+    - (i32)systemItem {
+        env.objc.borrow::<UIBarButtonItemHostObject>(this).system_item
+    }
+
+    - (())dealloc {
+        let UIBarButtonItemHostObject {
+            superclass: _,
+            title,
+            style: _,
+            target,
+            action,
+            ..
+        } = std::mem::take(env.objc.borrow_mut(this));
+
+        log_dbg!("dealloc [(UIBarButtonItem*){:?} title {:?}, target {:?}, action {:?}]", this, title, target, action);
+        msg_super![env; this dealloc]
+    }
+
+    @end
+};

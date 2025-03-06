b88f2d9f9c164ff60ba22a9a2ddce34637bf36d6
apexad <1437332+apexad@users.noreply.github.com>
2025-03-05 21:20:28 -0700

    Implement NSProccessInfo with proper proccessInfo, add physicalMemory
    Change-Id: I2668222a52c0c95b6427ef5bd7d3986202d4c92b

--- src/frameworks/foundation/ns_process_info.rs
+++ src/frameworks/foundation/ns_process_info.rs
@@ -5,9 +5,25 @@
  */
 //! `NSProcessInfo`.
 
-use super::NSTimeInterval;
-use crate::objc::{objc_classes, ClassExports};
+use super::{NSTimeInterval, NSUInteger};
+use crate::objc::{id, msg, msg_class, objc_classes, ClassExports};
+use crate::Environment;
 use std::time::Instant;
+
+#[derive(Default)]
+pub struct State {
+    instance: Option<id>,
+}
+
+impl State {
+    fn get(env: &mut Environment) -> &mut Self {
+        &mut env.framework_state.foundation.ns_process_info
+    }
+}
+
+// using 2GB; see mocked memory data src/libc/mach_host.rs
+// TODO: If mach_host.rs is written better this should be moved
+pub const PHYSICAL_MEMORY: u32 = 2 * 1024 * 1024 * 1024;
 
 pub const CLASSES: ClassExports = objc_classes! {
 
@@ -15,8 +31,25 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 @implementation NSProcessInfo: NSObject
 
-+ (NSTimeInterval)systemUptime {
++ (id)processInfo {
+    if let Some(ns_process_info_instance) = State::get(env).instance {
+        return ns_process_info_instance;
+    }
+
+    let new_obj: id = msg_class![env; NSProcessInfo alloc];
+    let new_obj: id = msg![env; new_obj init];
+
+    State::get(env).instance = Some(new_obj);
+
+    new_obj
+}
+
+- (NSTimeInterval)systemUptime {
     Instant::now().duration_since(env.startup_time).as_secs_f64()
+}
+
+- (NSUInteger)physicalMemory {
+    PHYSICAL_MEMORY
 }
 
 @end

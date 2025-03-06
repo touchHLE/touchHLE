83952b40f759afacda370f1a57b48a8939745687
apexad <1437332+apexad@users.noreply.github.com>
2025-03-05 21:20:28 -0700

    Add proccessInfo and physicalMemory functions to NSProcessInfo
    Change-Id: I2668222a52c0c95b6427ef5bd7d3986202d4c92b

--- src/frameworks/foundation/ns_process_info.rs
+++ src/frameworks/foundation/ns_process_info.rs
@@ -5,18 +5,37 @@
  */
 //! `NSProcessInfo`.
 
-use super::NSTimeInterval;
-use crate::objc::{objc_classes, ClassExports};
+use super::{NSTimeInterval, NSUInteger};
+use crate::objc::{id, objc_classes, ClassExports};
 use std::time::Instant;
 
+// using 2GB; see mocked memory data src/libc/mach_host.rs
+// TODO: If mach_host.rs is written better this should be moved
+pub const PHYSICAL_MEMORY: u32 = 2 * 1024 * 1024 * 1024;
 pub const CLASSES: ClassExports = objc_classes! {
 
 (env, this, _cmd);
 
 @implementation NSProcessInfo: NSObject
 
+// TODO: Fix this implentation so that it correctly uses instances
+// touchHLE impliments NSProcessInfo in a hacky way
+// There is no instance of the class returned, just the class itself
+// So methods are implimented as class methods not instance methods
+
++ (id)processInfo {
+    this.cast()
+}
+
+// systemUptime is supposed to be an *instance* method
+// returning uptime since boot in seconds.
 + (NSTimeInterval)systemUptime {
     Instant::now().duration_since(env.startup_time).as_secs_f64()
+}
+
+// physicalMemory is supposed to be an *instance* method
++ (NSUInteger)physicalMemory {
+    PHYSICAL_MEMORY
 }
 
 @end

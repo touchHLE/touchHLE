d450a7dd5743d94d51d8d7fef40321a638f06236
abnormalmaps <abnormalmaps@gmail.com>
2025-03-09 19:26:50 -0400

    Refactor GLES context management
    Changes the GLES contexts so that host contexts cannot be used until
    activated, which hands out a helper object that ensures the provided
    context is the only active context.
    
    Change-Id: I8519665ce8854c848771b794a5b3416517ff7e3f

--- src/frameworks/opengles/gles_guest.rs
+++ src/frameworks/opengles/gles_guest.rs
@@ -19,8 +19,7 @@ use touchHLE_gl_bindings::gles11::{
 
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::frameworks::opengles::eagl::EAGLContextHostObject;
-use crate::gles::gles11_raw as gles11; // constants only
-use crate::gles::GLES;
+use crate::gles::{gles11_raw as gles11, GLES}; // constants only
 use crate::mem::{ConstPtr, ConstVoidPtr, GuestISize, GuestUSize, Mem, MutPtr, MutVoidPtr, Ptr};
 use crate::objc::nil;
 use crate::Environment;
@@ -79,7 +78,7 @@ where
         return U::default();
     }
 
-    let gles = super::sync_context(
+    let mut gles = super::sync_context(
         &mut env.framework_state.opengles,
         &mut env.objc,
         env.window
@@ -89,7 +88,7 @@ where
     );
 
     //panic_on_gl_errors(&mut *gles);
-    let res = f(gles, &mut env.mem);
+    let res = f(gles.as_mut(), &mut env.mem);
     //panic_on_gl_errors(&mut *gles);
     #[allow(clippy::let_and_return)]
     res
@@ -104,7 +103,7 @@ fn with_ctx_and_mem_no_skip<T, U>(env: &mut Environment, f: T) -> U
 where
     T: FnOnce(&mut dyn GLES, &mut Mem) -> U,
 {
-    let gles = super::sync_context(
+    let mut gles = super::sync_context(
         &mut env.framework_state.opengles,
         &mut env.objc,
         env.window
@@ -113,9 +112,9 @@ where
         env.current_thread,
     );
 
-    //panic_on_gl_errors(&mut *gles);
-    let res = f(gles, &mut env.mem);
-    //panic_on_gl_errors(&mut *gles);
+    //panic_on_gl_errors(&mut **gles);
+    let res = f(gles.as_mut(), &mut env.mem);
+    //panic_on_gl_errors(&mut **gles);
     #[allow(clippy::let_and_return)]
     res
 }

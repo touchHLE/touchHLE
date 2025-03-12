a67c564147971c4d7d257bc300a57500c318f9e8
abnormalmaps <abnormalmaps@gmail.com>
2025-03-12 18:29:24 -0400

    Switch to coroutine based threading system
    Switch the threading system such that each guest thread has one
    corresponding host (stackful) coroutine.
    
    Change-Id: I9488116c9b9904beba3e47169fbc60b2c02522a2

--- src/frameworks/openal.rs
+++ src/frameworks/openal.rs
@@ -53,7 +53,7 @@ impl State {
             .contexts
             .get_mut(&state.current_ctx)
             .unwrap()
-            .make_current(&mut env.openal_manager)
+            .make_current(env.openal_manager.as_mut())
     }
 }
 
@@ -202,7 +202,7 @@ fn alcCreateContext(
 
     let res = unsafe {
         OpenALContext::new_with_device_and_attrlist(
-            &mut env.openal_manager,
+            env.openal_manager.as_mut(),
             host_device,
             attr_list_ptr,
         )
@@ -334,7 +334,7 @@ fn alGetEnumValue(env: &mut Environment, enumName: ConstPtr<u8>) -> ALenum {
     let state = &mut env.framework_state.openal;
     let context = state
         .contexts
-        .make_current(state.current_ctx, &mut env.openal_manager);
+        .make_current(state.current_ctx, env.openal_manager.as_mut());
     let res = unsafe { context.GetEnumValue(ss.as_ptr()) };
     log_dbg!("alGetEnumValue({:?}) => {:?}", s, res);
     res
@@ -649,7 +649,7 @@ fn alSourceUnqueueBuffers(
     let state = &mut env.framework_state.openal;
     let context = state
         .contexts
-        .make_current(state.current_ctx, &mut env.openal_manager);
+        .make_current(state.current_ctx, env.openal_manager.as_mut());
     let buffers_processed = {
         let mut val = 0;
         unsafe { context.GetSourcei(source, al::AL_BUFFERS_PROCESSED, &mut val) };

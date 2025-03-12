5f752f8e9d4ea50d17ca260bbb88e2bee1efd675
apexad <1437332+apexad@users.noreply.github.com>
2025-03-12 13:50:34 -0700

    Add a new option: "--force-no-status-bar" (defaults to false)
    Change-Id: I88ac0892e5b4638ca3bd68b5ccf296d6b7da2fa0

--- src/frameworks/uikit/ui_screen.rs
+++ src/frameworks/uikit/ui_screen.rs
@@ -51,7 +51,7 @@ pub const CLASSES: ClassExports = objc_classes! {
 - (CGRect)applicationFrame {
     let mut bounds: CGRect = msg![env; this bounds];
     const STATUS_BAR_HEIGHT: f32 = 20.0;
-    if !env.framework_state.uikit.ui_application.status_bar_hidden {
+    if !env.options.force_no_status_bar && !env.framework_state.uikit.ui_application.status_bar_hidden {
         bounds.origin.y += STATUS_BAR_HEIGHT;
         bounds.size.height -= STATUS_BAR_HEIGHT;
     }

bb496400745c9e4453526775e7745a874823ff9c
abnormalmaps <abnormalmaps@gmail.com>
2025-04-16 23:23:58 -0400

    Add --dump=symbols flag
    Adds the --dump=symbol flag, which is used to dump symbols for
    generating our version of libSystem.dylib.
    
    Change-Id: If255c3fd1d7dc4e3e5ae997553891296472ec39f

--- src/objc/classes.rs
+++ src/objc/classes.rs
@@ -838,6 +838,17 @@ impl ObjC {
         writeln!(file, "    ]\n}}")
     }
 
+    pub fn dump_host_class_symbols(file: &mut std::fs::File) -> Result<(), std::io::Error> {
+        use std::io::Write;
+        for (class, _) in CLASS_LISTS.iter().flat_map(|class| class.iter()) {
+            writeln!(file, "_OBJC_CLASS_$_{}", class)?;
+            writeln!(file, "_OBJC_METACLASS_$_{}", class)?;
+        }
+        writeln!(file, "__objc_empty_cache")?;
+        writeln!(file, "__objc_empty_vtable")?;
+        Ok(())
+    }
+
     /// For use by [crate::dyld]: register all the categories from the
     /// application binary.
     pub fn register_bin_categories(&mut self, bin: &MachO, mem: &mut Mem) {

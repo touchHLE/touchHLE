a953c7b717f1c82e35e033adaa03c52a4f65207d
kouta <darkfm@vera.com.uy>
2025-05-05 20:52:16 -0300

    To help Cause of Death boot: - Fake table generated where codepoint => codepoint instead of glyphId

--- src/frameworks/core_graphics/cg_font.rs
+++ src/frameworks/core_graphics/cg_font.rs
@@ -15,7 +15,96 @@ use crate::frameworks::core_graphics::cg_context::CGContextHostObject;
 use crate::frameworks::foundation::ns_string::{from_rust_string, to_rust_string};
 use crate::frameworks::uikit::ui_graphics::UIGraphicsGetCurrentContext;
 use crate::mem::{ConstPtr, GuestUSize, MutPtr};
+pub mod CMap {
+    use symphonia::core::meta::StandardTagKey::EncodedBy;
+    use crate::font::Font;
 
+    #[derive(Clone)]
+    pub struct Table {
+        pub version: u16,
+        pub subtable_count: u16,
+        pub encoding_records: Vec<EncodingRecord>,
+        pub subtables: Vec<Subtable>,
+    }
+
+    #[derive(Clone)]
+    pub struct EncodingRecord {
+        pub platform_id: u16,
+        pub platform_specific_id: u16,
+        pub offset: u32,
+    }
+
+    #[derive(Clone)]
+    pub struct SubtableFormat6 {
+        pub format: u16,
+        pub length: u16,
+        pub language: u16,
+        pub first_code: u16,
+        pub entry_count: u16,
+        pub glyph_index_array: Vec<u16>,
+    }
+
+    #[derive(Clone)]
+    pub enum Subtable {
+        Format6(SubtableFormat6)
+    }
+
+    pub fn table_from_font(_font: &Font) -> Table {
+        let ascii_range = (0u8..255u8);
+        let codepoints: Vec<u16> = ascii_range
+            .map(|cp| cp as u16)
+            .collect();
+        Table {
+            version: 0,
+            subtable_count: 1,
+            encoding_records: [
+                EncodingRecord {
+                    platform_id: 0,
+                    platform_specific_id: 3,
+                    offset: (size_of::<u16>()*2 + size_of::<EncodingRecord>()) as u32
+                }
+            ].to_vec(),
+            subtables: [
+                Subtable::Format6(
+                    SubtableFormat6{
+                        format: 6,
+                        length: 256*2,
+                        language: 0,
+                        first_code: 0,
+                        entry_count: codepoints.len() as u16,
+                        glyph_index_array: codepoints
+                    }
+                )
+            ].to_vec()
+        }
+    }
+
+    pub fn serialize(table: &Table) -> Vec<u8> {
+        let mut buf = Vec::new();
+        buf.extend_from_slice(&table.version.to_be_bytes());
+        buf.extend_from_slice(&table.subtable_count.to_be_bytes());
+        for record in table.encoding_records.iter() {
+            buf.extend_from_slice(&record.platform_id.to_be_bytes());
+            buf.extend_from_slice(&record.platform_specific_id.to_be_bytes());
+            buf.extend_from_slice(&record.offset.to_be_bytes());
+        }
+        for subtable in table.subtables.iter() {
+            match subtable {
+                Subtable::Format6(format6) => {
+                    buf.extend_from_slice(&format6.format.to_be_bytes());
+                    buf.extend_from_slice(&format6.length.to_be_bytes());
+                    buf.extend_from_slice(&format6.language.to_be_bytes());
+                    buf.extend_from_slice(&format6.first_code.to_be_bytes());
+                    buf.extend_from_slice(&format6.entry_count.to_be_bytes());
+                    for &glyph_id in &format6.glyph_index_array {
+                        buf.extend_from_slice(&glyph_id.to_be_bytes());
+                    }
+                }
+            }
+        }
+        buf
+    }
+}
 pub type CGFontRef = CFTypeRef;
 pub struct CGFontHostObject {
     font: Font,
@@ -69,7 +158,7 @@ pub fn glyphs_at_point(
 
 pub fn CGFontCreateWithFontName(env: &mut Environment, name: CFStringRef) -> CGFontRef {
     let host_obj = Box::new(CGFontHostObject {
-        font: Font::serif_regular() // TODO actually load requested font.
+        font: Font::sans_regular() // TODO actually load requested font.
     });
     let name = to_rust_string(env, name);
     log!("Ignoring CGFontCreateWithFontName((CFString*){:?})", name);
@@ -80,7 +169,7 @@ pub fn CGFontCreateWithFontName(env: &mut Environment, name: CFStringRef) -> CGF
 pub fn CGFontCreateWithDataProvider(env: &mut Environment, provider: CGDataProviderRef) -> CGFontRef {
     log!("Ignoring CGFontCreateWithDataProvider((CGDataProvider*){:?})", provider);
     let host_obj = Box::new(CGFontHostObject {
-        font: Font::serif_regular() // TODO actually load requested font.
+        font: Font::sans_regular() // TODO actually load requested font.
     });
     let class = env.objc.get_known_class("CGFont", &mut env.mem);
     env.objc.alloc_object(class, host_obj, &mut env.mem)
@@ -88,7 +177,7 @@ pub fn CGFontCreateWithDataProvider(env: &mut Environment, provider: CGDataProvi
 
 pub fn CGFontCopyTableForTag(env: &mut Environment, font: CGFontRef, tag: u32) -> CFDataRef {
     let font = env.objc.borrow::<CGFontHostObject>(font);
-    let table_bytes = font.font.get_raw_table(tag);
+    let table_bytes = CMap::serialize(&CMap::table_from_font(&font.font));
     let table_bytes_guest = env.mem.alloc(table_bytes.len() as GuestUSize).cast();
     for i in 0..table_bytes.len() {
         env.mem.write::<u8>(table_bytes_guest + i as GuestUSize, table_bytes[i]);

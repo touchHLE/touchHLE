6de976b27aaa8dbc77ba15387f9020935aaa90f7
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-06-04 16:04:34 -0600

    Increase amount of invalid cap accepted by glDisable
    Allow defined parameters to be accepted as invalid capabilities
    
    Change-Id: I85b796cc6d5934ffd3d3750bc1199b32b17c6299

--- src/gles/gles1_on_gl2.rs
+++ src/gles/gles1_on_gl2.rs
@@ -280,6 +280,7 @@ const GET_PARAMS: ParamTable = ParamTable(&[
     (gl21::MAX_PALETTE_MATRICES_ARB, ParamType::Int, 1),
     // OES_matrix_palette -> ARB_vertex_blend
     (gl21::MAX_VERTEX_UNITS_ARB, ParamType::Int, 1),
+    (gl21::COMPRESSED_TEXTURE_FORMATS, ParamType::Int, 0),
 ]);
 
 const POINT_PARAMS: ParamTable = ParamTable(&[
@@ -614,16 +615,16 @@ impl GLES for GLES1OnGL2 {
         gl21::IsEnabled(cap)
     }
     unsafe fn Disable(&mut self, cap: GLenum) {
-        if ARRAYS.iter().any(|&ArrayInfo { name, .. }| name == cap) {
+        if CAPABILITIES.contains(&cap) {
+            log_dbg!("glDisable{:#x}", cap);
+        } else if ARRAYS.iter().any(|&ArrayInfo { name, .. }| name == cap) {
             log_dbg!("Tolerating glDisable({:#x}) of client state", cap);
         } else if UNSUPPORTED_CAPABILITIES.contains(&cap) {
             log_dbg!("Tolerating glDisable({:#x}) of unsupported capability", cap);
+        } else if GET_PARAMS.is_known_param(cap) {
+            log_dbg!("Tolerating glDisable({:#x}) of parameter", cap);
         } else {
-            assert!(
-                CAPABILITIES.contains(&cap),
-                "Unexpected glDisable({:#x})",
-                cap
-            );
+            panic!("Unexpected glDisable({:#x})", cap);
         }
         gl21::Disable(cap);
     }

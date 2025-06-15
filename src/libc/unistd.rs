5fee43fa93f0149474611de672b0fd0486dccd37
ciciplusplus <ciciplusplus@gmail.com>
2025-06-16 00:12:11 +0200

    Implement page size alignment in alloc
    Change-Id: I9b02c685f93d6d28752b3b8ca580bb3afda25358

--- src/libc/unistd.rs
+++ src/libc/unistd.rs
@@ -8,9 +8,8 @@
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::fs::GuestPath;
 use crate::libc::errno::set_errno;
-use crate::libc::mach_host::PAGE_SIZE;
 use crate::libc::posix_io::{FileDescriptor, STDERR_FILENO, STDIN_FILENO, STDOUT_FILENO};
-use crate::mem::{ConstPtr, GuestUSize, MutPtr};
+use crate::mem::{ConstPtr, GuestUSize, MutPtr, PAGE_SIZE};
 use crate::Environment;
 use std::time::Duration;
 

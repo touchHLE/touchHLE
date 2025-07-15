a334d4cd127792ff94a3620470d98651a4ecaad0
ciciplusplus <ciciplusplus@gmail.com>
2025-07-16 00:22:50 +0200

    Implement CFNumberGetValue() and CFBooleanGetValue()
    Also implement NSNumber isEqualToNumber: method
    
    Change-Id: I8be7caa048423e76b7a39b0a9fce9d4fbfc2dbd0

--- src/frameworks/foundation/ns_value.rs
+++ src/frameworks/foundation/ns_value.rs
@@ -6,6 +6,10 @@
 //! The `NSValue` class cluster, including `NSNumber`.
 
 use super::NSUInteger;
+use crate::frameworks::core_foundation::cf_number::{
+    kCFNumberCharType, kCFNumberFloat32Type, kCFNumberFloatType, kCFNumberIntType,
+    kCFNumberSInt16Type, kCFNumberSInt32Type, kCFNumberSInt8Type, kCFNumberShortType, CFNumberType,
+};
 use crate::frameworks::foundation::ns_string::from_rust_string;
 use crate::frameworks::foundation::NSInteger;
 use crate::mem::{ConstVoidPtr, MutVoidPtr};
@@ -320,7 +324,30 @@ pub const CLASSES: ClassExports = objc_classes! {
 }
 
 - (bool)isEqual:(id)other {
-    equality_helper(env, this, other)
+    if this == other {
+        return true;
+    }
+    let class: Class = msg_class![env; NSNumber class];
+    if !msg![env; other isKindOfClass:class] {
+        return false;
+    }
+    msg![env; this isEqualToNumber:other]
+}
+
+- (bool)isEqualToNumber:(id)other {
+    let num = env.objc.borrow::<NSNumberHostObject>(this);
+    let other_num = env.objc.borrow::<NSNumberHostObject>(other);
+    match num {
+        NSNumberHostObject::Bool(value) => *value as u64 == other_num.as_unsigned_long_long(),
+        NSNumberHostObject::UnsignedLongLong(value) => *value == other_num.as_unsigned_long_long(),
+        NSNumberHostObject::UnsignedInt(value) => *value as u64 == other_num.as_unsigned_long_long(),
+        NSNumberHostObject::Int(value) => *value as i64 == other_num.as_long_long(),
+        NSNumberHostObject::LongLong(value) => *value == other_num.as_long_long(),
+        NSNumberHostObject::Float(value) => *value as f64 == other_num.as_double(),
+        NSNumberHostObject::Double(value) => *value == other_num.as_double(),
+        NSNumberHostObject::Short(value) => *value as i64 == other_num.as_long_long(),
+        NSNumberHostObject::Char(value) => *value as i64 == other_num.as_long_long(),
+    }
 }
 
 // TODO: accessors etc
@@ -329,29 +356,26 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 };
 
-fn equality_helper(env: &mut Environment, this: id, other: id) -> bool {
-    if this == other {
-        return true;
-    }
-    let class: Class = msg_class![env; NSNumber class];
-    if !msg![env; other isKindOfClass:class] {
-        return false;
-    }
-    let (left, right) = (env.objc.borrow(this), env.objc.borrow(other));
-    match (left, right) {
-        (&NSNumberHostObject::Bool(a), &NSNumberHostObject::Bool(b)) => a == b,
-        (&NSNumberHostObject::UnsignedLongLong(a), &NSNumberHostObject::UnsignedLongLong(b)) => {
-            a == b
+pub fn is_conversion_lossless(env: &mut Environment, this: id, type_: CFNumberType) -> bool {
+    let num = env.objc.borrow::<NSNumberHostObject>(this);
+    let num2: id = match type_ {
+        kCFNumberSInt32Type | kCFNumberIntType => {
+            let val: i32 = num.as_int();
+            msg_class![env; NSNumber numberWithInt:val]
         }
-        (&NSNumberHostObject::UnsignedInt(a), &NSNumberHostObject::UnsignedInt(b)) => a == b,
-        (&NSNumberHostObject::Int(a), &NSNumberHostObject::Int(b)) => a == b,
-        (&NSNumberHostObject::LongLong(a), &NSNumberHostObject::LongLong(b)) => a == b,
-        (&NSNumberHostObject::Float(a), &NSNumberHostObject::Float(b)) => a == b,
-        (&NSNumberHostObject::Double(a), &NSNumberHostObject::Double(b)) => a == b,
-        _ => todo!(
-            "Implement NSNumber comparisons of different types: {:?} vs {:?}",
-            left,
-            right
-        ),
-    }
+        kCFNumberFloat32Type | kCFNumberFloatType => {
+            let val: f32 = num.as_float();
+            msg_class![env; NSNumber numberWithFloat:val]
+        }
+        kCFNumberSInt16Type | kCFNumberShortType => {
+            let val: i16 = num.as_short();
+            msg_class![env; NSNumber numberWithShort:val]
+        }
+        kCFNumberSInt8Type | kCFNumberCharType => {
+            let val: i8 = num.as_char();
+            msg_class![env; NSNumber numberWithChar:val]
+        }
+        _ => unimplemented!("is_conversion_lossless for {}", type_),
+    };
+    msg![env; this isEqualToNumber:num2]
 }

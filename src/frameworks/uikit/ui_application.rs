883f36a1048e8b01dea8be21b917039a9f67d383
apexad <1437332+apexad@users.noreply.github.com>
2025-07-17 19:10:27 -0600

    Convert UIApplication windows log to log_dbg
    Change-Id: I382a87cc67d39a9470b556d6973b575919db81a9

--- src/frameworks/uikit/ui_application.rs
+++ src/frameworks/uikit/ui_application.rs
@@ -165,7 +165,8 @@ pub const CLASSES: ClassExports = objc_classes! {
 }
 
 - (id)windows {
-    log!("TODO: UIApplication's windows getter is returning only visible windows");
+    // very spammy in some games (example: DOA Blacjack), so using log_dbg
+    log_dbg!("TODO: UIApplication's windows getter is returning only visible windows");
     let visible_windows: Vec<id> = (*env
         .framework_state
         .uikit

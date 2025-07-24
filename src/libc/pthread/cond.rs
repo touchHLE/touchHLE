6c77210d955dfda2fd3875ade1268a62b71c6750
abnormalmaps <abnormalmaps@gmail.com>
2025-07-24 17:58:02 -0400

    Fix pthread_cond_signal, add pthread_cond_broadcast
    Before this commit, pthread_cond_signal signalled all threads and
    remained signalled after it was signaled for the first time. This commit
    fixes this behaviour to only wake up waiting threads, and adds
    pthread_cond_broadcast based on the new implementation.
    
    Change-Id: I16ee75a09d2e064d258fcc1c981c8987805343e5

--- src/libc/pthread/cond.rs
+++ src/libc/pthread/cond.rs
@@ -10,12 +10,12 @@ use crate::dyld::FunctionExports;
 use crate::libc::pthread::mutex::pthread_mutex_unlock;
 use crate::mem::{ConstPtr, MutPtr, SafeRead};
 use crate::{export_c_func, Environment};
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
-use crate::environment::ThreadBlock;
+use crate::environment::{MutexId, ThreadBlock, ThreadId};
 
 #[repr(C, packed)]
-struct pthread_condattr_t {}
+pub struct pthread_condattr_t {}
 unsafe impl SafeRead for pthread_condattr_t {}
 
 #[repr(C, packed)]
@@ -29,7 +29,6 @@ pub type pthread_cond_t = MutPtr<OpaqueCond>;
 #[derive(Default)]
 pub struct State {
     pub condition_variables: HashMap<pthread_cond_t, CondHostObject>,
-    pub mutexes: HashMap<pthread_cond_t, pthread_mutex_t>,
 }
 impl State {
     fn get(env: &Environment) -> &Self {
@@ -41,10 +40,12 @@ impl State {
 }
 
 pub struct CondHostObject {
-    pub done: bool,
+    waiting: VecDeque<ThreadId>,
+    pub(crate) waking: VecDeque<ThreadId>,
+    pub(crate) curr_mutex: Option<MutexId>,
 }
 
-fn pthread_cond_init(
+pub fn pthread_cond_init(
     env: &mut Environment,
     cond: MutPtr<pthread_cond_t>,
     attr: ConstPtr<pthread_condattr_t>,
@@ -54,13 +55,18 @@ fn pthread_cond_init(
     env.mem.write(cond, opaque);
 
     assert!(!State::get(env).condition_variables.contains_key(&opaque));
-    State::get_mut(env)
-        .condition_variables
-        .insert(opaque, CondHostObject { done: false });
+    State::get_mut(env).condition_variables.insert(
+        opaque,
+        CondHostObject {
+            waiting: VecDeque::new(),
+            waking: VecDeque::new(),
+            curr_mutex: None,
+        },
+    );
     0 // success
 }
 
-fn pthread_cond_wait(
+pub fn pthread_cond_wait(
     env: &mut Environment,
     cond: MutPtr<pthread_cond_t>,
     mutex: MutPtr<pthread_mutex_t>,
@@ -76,33 +82,71 @@ fn pthread_cond_wait(
         env.current_thread,
         cond
     );
+    let current_thread = env.current_thread;
+    let mutex = env.mem.read(mutex).mutex_id;
     let cond_var = env.mem.read(cond);
+    let host_object = State::get_mut(env)
+        .condition_variables
+        .get_mut(&cond_var)
+        .unwrap();
+    // The mutex used must be the same as the currently waiting mutex, or there
+    // must be no other waiters.
+    assert!(
+        host_object.curr_mutex == Some(mutex)
+            || host_object.waking.is_empty() && host_object.waiting.is_empty()
+    );
+    host_object.curr_mutex = Some(mutex);
+    host_object.waiting.push_back(current_thread);
     env.threads[env.current_thread].blocked_by = ThreadBlock::Condition(cond_var);
-    assert!(!State::get(env).mutexes.contains_key(&cond_var));
-    let mutex_val = env.mem.read(mutex);
-    State::get_mut(env).mutexes.insert(cond_var, mutex_val);
     0 // success
 }
 
-fn pthread_cond_signal(env: &mut Environment, cond: MutPtr<pthread_cond_t>) -> i32 {
+pub fn pthread_cond_signal(env: &mut Environment, cond: MutPtr<pthread_cond_t>) -> i32 {
+    let cond_var = env.mem.read(cond);
+    let host_object = State::get_mut(env)
+        .condition_variables
+        .get_mut(&cond_var)
+        .unwrap();
+    if let Some(tid) = host_object.waiting.pop_front() {
+        host_object.waking.push_back(tid);
+        log_dbg!(
+            "Thread {} unblocks one thread ({}) waiting on condition variable {:?}",
+            env.current_thread,
+            tid,
+            cond
+        );
+    } else {
+        log_dbg!(
+            "Thread {} signals condition variable {:?}, no waiters",
+            env.current_thread,
+            cond
+        );
+    }
+    0 // success
+}
+
+pub fn pthread_cond_broadcast(env: &mut Environment, cond: MutPtr<pthread_cond_t>) -> i32 {
     let cond_var = env.mem.read(cond);
     log_dbg!(
         "Thread {} unblocks one thread waiting on condition variable {:?}",
         env.current_thread,
         cond
     );
-    State::get_mut(env)
+    let host_object = State::get_mut(env)
         .condition_variables
         .get_mut(&cond_var)
-        .unwrap()
-        .done = true;
+        .unwrap();
+    host_object.waking.extend(host_object.waiting.drain(..));
     0 // success
 }
 
-fn pthread_cond_destroy(env: &mut Environment, cond: MutPtr<pthread_cond_t>) -> i32 {
+pub fn pthread_cond_destroy(env: &mut Environment, cond: MutPtr<pthread_cond_t>) -> i32 {
     let cond_var = env.mem.read(cond);
-    State::get_mut(env).condition_variables.remove(&cond_var);
-    State::get_mut(env).mutexes.remove(&cond_var);
+    let old_object = State::get_mut(env)
+        .condition_variables
+        .remove(&cond_var)
+        .unwrap();
+    assert!(old_object.waiting.is_empty() && old_object.waking.is_empty());
     env.mem.free(cond_var.cast());
     0 // success
 }
@@ -111,5 +155,6 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(pthread_cond_init(_, _)),
     export_c_func!(pthread_cond_wait(_, _)),
     export_c_func!(pthread_cond_signal(_)),
+    export_c_func!(pthread_cond_broadcast(_)),
     export_c_func!(pthread_cond_destroy(_)),
 ];

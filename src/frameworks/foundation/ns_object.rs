314955538287b0e0c6a2dc5e866d419f2dd9ebd8
alborrajo <alvarolopezborr@gmail.com>
2025-08-04 23:09:06 +0200

    Game specific hack for RE Degeneration
    Change-Id: I45e9e1202026e9006425bc4b6cedf4a67b0a0f94

--- src/frameworks/foundation/ns_object.rs
+++ src/frameworks/foundation/ns_object.rs
@@ -313,6 +313,23 @@ forUndefinedKey:(id)key { // NSString*
             return;
         }
     }
+    if env.bundle.bundle_identifier().starts_with("jp.co.capcom.residentevil.en") && wait {
+        log!("Applying game-specific hack for RE Degeneration: ignoring performSelectorOnMainThread:SEL({}) waitUntilDone:true", sel.as_str(&env.mem));
+        let old_thread = env.current_thread;
+        if env.current_thread != 0 {
+            env.switch_thread(0);
+        }
+        if sel.as_str(&env.mem).ends_with(':') {
+            () = msg_send(env, (this, sel, arg));
+        } else {
+            assert!(arg.is_null());
+            () = msg_send(env, (this, sel));
+        }
+        if env.current_thread != old_thread {
+            env.switch_thread(old_thread);
+        }
+        return;
+    }
     // TODO: support waiting
     // This would require tail calls for message send or a switch to async model
     assert!(!wait);

c64715f61b7b3980c65f36bcd43d5c3d0f082e6b
abnormalmaps <abnormalmaps@gmail.com>
2025-08-13 12:16:27 -0400

    Round UITouch coordinates
    This matches the behaviour seen on actual devices, where only integer
    coordinates are allowed.
    
    Change-Id: I568b3d5a8a3c49743dd354f9e16bec45f168343e

--- src/frameworks/uikit/ui_touch.rs
+++ src/frameworks/uikit/ui_touch.rs
@@ -163,8 +163,8 @@ fn handle_touches_down(env: &mut Environment, map: HashMap<FingerId, Coords>) {
         log_dbg!("Finger {:?} touch down: {:?}", finger_id, coords);
 
         let location = CGPoint {
-            x: coords.0,
-            y: coords.1,
+            x: coords.0.round(),
+            y: coords.1.round(),
         };
 
         // Check if this tap is a double tap.
@@ -379,8 +379,8 @@ fn handle_touches_move(env: &mut Environment, map: HashMap<FingerId, Coords>) {
         };
 
         let location = CGPoint {
-            x: coords.0,
-            y: coords.1,
+            x: coords.0.round(),
+            y: coords.1.round(),
         };
 
         let view = env.objc.borrow::<UITouchHostObject>(touch).view;
@@ -484,8 +484,8 @@ fn handle_touches_up(env: &mut Environment, map: HashMap<FingerId, Coords>) {
         log_dbg!("Finger {:?} touch up: {:?}", finger_id, coords);
 
         let location = CGPoint {
-            x: coords.0,
-            y: coords.1,
+            x: coords.0.round(),
+            y: coords.1.round(),
         };
 
         let view = env.objc.borrow::<UITouchHostObject>(touch).view;

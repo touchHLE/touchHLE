86be503bd7263a18d4994e475126f07b468f1984
abnormalmaps <abnormalmaps@gmail.com>
2025-08-27 16:54:37 -0400

    Implement CGRectNull
    Change-Id: I837dce5bd0048e5866e37f0e5227ca9dfa14d8e1

--- src/frameworks/core_graphics/cg_geometry.rs
+++ src/frameworks/core_graphics/cg_geometry.rs
@@ -222,6 +222,18 @@ fn CGRectMake(
     }
 }
 
+pub const CGRectNull: CGRect = CGRect {
+    origin: CGPoint {
+        x: f32::INFINITY,
+        y: f32::INFINITY,
+    },
+    size: CGSizeZero,
+};
+
+fn CGRectIsNull(_env: &mut Environment, rect: CGRect) -> bool {
+    rect == CGRectNull
+}
+
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CGPointEqualToPoint(_, _)),
     export_c_func!(CGSizeEqualToSize(_, _)),
@@ -235,6 +247,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CGRectGetHeight(_)),
     export_c_func!(CGRectGetWidth(_)),
     export_c_func!(CGRectMake(_, _, _, _)),
+    export_c_func!(CGRectIsNull(_)),
 ];
 
 pub const CONSTANTS: ConstantExports = &[
@@ -249,5 +262,9 @@ pub const CONSTANTS: ConstantExports = &[
     (
         "_CGRectZero",
         HostConstant::Custom(|env| env.mem.alloc_and_write(CGRectZero).cast().cast_const()),
+    ),
+    (
+        "_CGRectNull",
+        HostConstant::Custom(|env| env.mem.alloc_and_write(CGRectNull).cast().cast_const()),
     ),
 ];

b31eb2437a25b48f1ef941838743258c9e50c440
jasugames <jasugames@gmail.com>
2025-09-01 18:41:27 +0200

    fix: gl21::BYTE added to assert exceptions in GLES renderer
    Change-Id: I7a205b10749542ab20cbf739872f39dcde38f51a

--- src/gles/gles1_on_gl2.rs
+++ src/gles/gles1_on_gl2.rs
@@ -1160,8 +1160,7 @@ impl GLES for GLES1OnGL2 {
             self.pointer_is_fixed_point[2] = true;
             gl21::TexCoordPointer(size, gl21::FLOAT, stride, pointer)
         } else {
-            // TODO: byte
-            assert!(type_ == gl21::SHORT || type_ == gl21::FLOAT);
+            assert!(type_ == gl21::BYTE || type_ == gl21::SHORT || type_ == gl21::FLOAT);
             self.fixed_point_texture_units.remove(&active_texture);
             if self.fixed_point_texture_units.is_empty() {
                 self.pointer_is_fixed_point[2] = false;
@@ -1182,8 +1181,7 @@ impl GLES for GLES1OnGL2 {
             self.pointer_is_fixed_point[3] = true;
             gl21::VertexPointer(size, gl21::FLOAT, stride, pointer)
         } else {
-            // TODO: byte
-            assert!(type_ == gl21::SHORT || type_ == gl21::FLOAT);
+            assert!(type_ == gl21::BYTE || type_ == gl21::SHORT || type_ == gl21::FLOAT);
             self.pointer_is_fixed_point[3] = false;
             gl21::VertexPointer(size, type_, stride, pointer)
         }

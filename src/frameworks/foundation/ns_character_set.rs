4cbb75ca7943ab4c4808db819b189204b8416b69
abnormalmaps <abnormalmaps@gmail.com>
2025-09-02 00:05:30 -0400

    Add -[NSCharacterSet controlCharacterSet]
    Change-Id: Id7f22def7863b7c0654cc5489ba4d94a1df25f49

--- src/frameworks/foundation/ns_character_set.rs
+++ src/frameworks/foundation/ns_character_set.rs
@@ -22,6 +22,27 @@ const NEWLINE_CHARACTERS: [char; 7] = [
     '\u{000A}', '\u{000B}', '\u{000C}', '\u{000D}', '\u{0085}', '\u{2028}', '\u{2029}',
 ];
 
+// Unicode General Category Cc and Cf.
+// TODO: This is not actually the full list of charadcters in Cc and Cf, but
+// these are the ones reported from a simulator run with all `unichar`s. This
+// excludes characters outside the BMP, whcih would only be needed when we
+// support -longCharacterIsMember:.
+const CONTROL_CHARACTERS: [char; 99] = [
+    '\u{0000}', '\u{0001}', '\u{0002}', '\u{0003}', '\u{0004}', '\u{0005}', '\u{0006}', '\u{0007}',
+    '\u{0008}', '\u{0009}', '\u{000A}', '\u{000B}', '\u{000C}', '\u{000D}', '\u{000E}', '\u{000F}',
+    '\u{0010}', '\u{0011}', '\u{0012}', '\u{0013}', '\u{0014}', '\u{0015}', '\u{0016}', '\u{0017}',
+    '\u{0018}', '\u{0019}', '\u{001A}', '\u{001B}', '\u{001C}', '\u{001D}', '\u{001E}', '\u{001F}',
+    '\u{007F}', '\u{0080}', '\u{0081}', '\u{0082}', '\u{0083}', '\u{0084}', '\u{0085}', '\u{0086}',
+    '\u{0087}', '\u{0088}', '\u{0089}', '\u{008A}', '\u{008B}', '\u{008C}', '\u{008D}', '\u{008E}',
+    '\u{008F}', '\u{0090}', '\u{0091}', '\u{0092}', '\u{0093}', '\u{0094}', '\u{0095}', '\u{0096}',
+    '\u{0097}', '\u{0098}', '\u{0099}', '\u{009A}', '\u{009B}', '\u{009C}', '\u{009D}', '\u{009E}',
+    '\u{009F}', '\u{00AD}', '\u{0600}', '\u{0601}', '\u{0602}', '\u{0603}', '\u{06DD}', '\u{070F}',
+    '\u{17B4}', '\u{17B5}', '\u{200B}', '\u{200C}', '\u{200D}', '\u{200E}', '\u{200F}', '\u{202A}',
+    '\u{202B}', '\u{202C}', '\u{202D}', '\u{202E}', '\u{2060}', '\u{2061}', '\u{2062}', '\u{2063}',
+    '\u{2064}', '\u{206A}', '\u{206B}', '\u{206C}', '\u{206D}', '\u{206E}', '\u{206F}', '\u{FEFF}',
+    '\u{FFF9}', '\u{FFFA}', '\u{FFFB}',
+];
+
 /// Belongs to _touchHLE_NSCharacterSet
 struct CharacterSetHostObject {
     set: HashSet<unichar>,
@@ -80,6 +101,15 @@ pub const CLASSES: ClassExports = objc_classes! {
     let set1 = HashSet::from(NEWLINE_CHARACTERS.map(|c| unichar::try_from(c).unwrap()));
     let set2 = HashSet::from(WHITESPACE_CHARACTERS.map(|c| unichar::try_from(c).unwrap()));
     let set = set1.union(&set2).copied().collect();
+
+    let new: id = msg![env; this alloc];
+    env.objc.borrow_mut::<CharacterSetHostObject>(new).set = set;
+
+    autorelease(env, new)
+}
+
++ (id)controlCharacterSet {
+    let set = HashSet::from(CONTROL_CHARACTERS.map(|c| unichar::try_from(c).unwrap()));
 
     let new: id = msg![env; this alloc];
     env.objc.borrow_mut::<CharacterSetHostObject>(new).set = set;

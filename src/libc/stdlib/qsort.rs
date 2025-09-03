15b80398a3d56c21bb5b090535e5da4bdc03937c
abnormalmaps <abnormalmaps@gmail.com>
2025-09-02 23:09:58 -0400

    Tolerate bad sort functions in [NSArray sort...]
    Allow for non-ordered sort functions in [NSArray sortUsingFunction:...].
    This is done by genericizing and reusing the qsort sorting function.
    
    Change-Id: I05aa01d005fadbc48e035c9111e14df29bdc6f26

--- src/libc/stdlib/qsort.rs
+++ src/libc/stdlib/qsort.rs
@@ -17,21 +17,55 @@ fn qsort(
     size: u32,
     compar: GuestFunction, // int (*compar)(const void *, const void*))
 ) {
+    qsort_generic(
+        env,
+        nitems,
+        &mut |env, i, j| {
+            let i_ptr = base + i * size;
+            let j_ptr = base + j * size;
+            compar.call_from_host(env, (i_ptr.cast_const(), j_ptr.cast_const()))
+        },
+        &mut |env: &mut Environment, mut i: u32, mut j: u32| {
+            if i == j {
+                return;
+            }
+            if i > j {
+                std::mem::swap(&mut i, &mut j);
+            }
+            let base_slice = env.mem.bytes_at_mut(base, nitems * size);
+            let (_, slice_after_first) = base_slice.split_at_mut((i * size) as usize);
+            let offset = (j * size - i * size) as usize;
+            let (left, right) = slice_after_first.split_at_mut(offset);
+            left[..size as usize].swap_with_slice(&mut right[..size as usize]);
+        },
+    );
+}
+
+pub fn qsort_generic<T, Compare, Swap>(
+    user_data: &mut T,
+    nitems: u32,
+    compar: &mut Compare, // int (*compar)(const void *, const void*))
+    swap: &mut Swap,
+) where
+    Compare: FnMut(&mut T, u32, u32) -> i32,
+    Swap: FnMut(&mut T, u32, u32),
+{
     if nitems < 2 {
         return;
     }
-    qsort_rec(env, base, nitems, size, compar, 0, nitems - 1);
+    qsort_rec(user_data, compar, swap, 0, nitems - 1);
 }
 
-fn qsort_rec(
-    env: &mut Environment,
-    base: MutPtr<u8>,
-    nitems: u32,
-    size: u32,
-    compar: GuestFunction,
+fn qsort_rec<T, Compare, Swap>(
+    user_data: &mut T,
+    compar: &mut Compare, // int (*compar)(const void *, const void*))
+    swap: &mut Swap,
     low: u32,
     hi: u32,
-) {
+) where
+    Compare: FnMut(&mut T, u32, u32) -> i32,
+    Swap: FnMut(&mut T, u32, u32),
+{
     if low >= hi {
         return;
     }
@@ -39,44 +73,16 @@ fn qsort_rec(
     let pivot = low;
     let mut separator = low + 1;
     for i in low + 1..=hi {
-        if compare(env, base, size, compar, i, pivot) < 0 {
-            swap_slices(env, base, nitems, size, i, separator);
+        if compar(user_data, i, pivot) < 0 {
+            swap(user_data, i, separator);
             separator += 1;
         }
     }
-    swap_slices(env, base, nitems, size, pivot, separator - 1);
+    swap(user_data, pivot, separator - 1);
     if separator > 1 {
-        qsort_rec(env, base, nitems, size, compar, low, separator - 2);
+        qsort_rec(user_data, compar, swap, low, separator - 2);
     }
-    qsort_rec(env, base, nitems, size, compar, separator, hi);
-}
-
-fn compare(
-    env: &mut Environment,
-    base: MutPtr<u8>,
-    size: u32,
-    compar: GuestFunction,
-    i: u32,
-    j: u32,
-) -> i32 {
-    let i_ptr = base + i * size;
-    let j_ptr = base + j * size;
-    compar.call_from_host(env, (i_ptr.cast_const(), j_ptr.cast_const()))
-}
-
-fn swap_slices(env: &mut Environment, base: MutPtr<u8>, nitems: u32, size: u32, i: u32, j: u32) {
-    if i > j {
-        swap_slices(env, base, nitems, size, j, i);
-        return;
-    }
-    if i == j {
-        return;
-    }
-    let base_slice = env.mem.bytes_at_mut(base, nitems * size);
-    let (_, slice_after_first) = base_slice.split_at_mut((i * size) as usize);
-    let offset = (j * size - i * size) as usize;
-    let (left, right) = slice_after_first.split_at_mut(offset);
-    left[..size as usize].swap_with_slice(&mut right[..size as usize]);
+    qsort_rec(user_data, compar, swap, separator, hi);
 }
 
 pub const FUNCTIONS: FunctionExports = &[export_c_func!(qsort(_, _, _, _))];

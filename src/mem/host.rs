b310c6cee59d34612dd1a60ece24efa4ec0fd851
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-09-11 13:53:35 +0300

    Allocate guest memory using platform specific calls
    Change-Id: Ic9fd3b790640bb0525c2ae425c26cc9fb922a843

---
+++ src/mem/host.rs
@@ -0,0 +1,73 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#[cfg(windows)]
+pub(crate) fn allocate_memory(size: usize) -> std::io::Result<*mut core::ffi::c_void> {
+    use windows_sys::Win32::System::Memory::{
+        VirtualAlloc, MEM_COMMIT, MEM_RESERVE, PAGE_READWRITE,
+    };
+
+    let ptr = unsafe {
+        VirtualAlloc(
+            std::ptr::null_mut(),
+            size,
+            MEM_RESERVE | MEM_COMMIT,
+            PAGE_READWRITE,
+        )
+    };
+
+    if ptr.is_null() {
+        return Err(std::io::Error::last_os_error());
+    }
+    Ok(ptr)
+}
+
+#[cfg(unix)]
+pub(crate) fn allocate_memory(size: usize) -> std::io::Result<*mut core::ffi::c_void> {
+    use libc::{mmap, MAP_ANONYMOUS, MAP_PRIVATE, PROT_READ, PROT_WRITE};
+
+    let ptr = unsafe {
+        mmap(
+            std::ptr::null_mut(),
+            size,
+            PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANONYMOUS,
+            -1,
+            0,
+        )
+    };
+
+    if ptr == libc::MAP_FAILED {
+        return Err(std::io::Error::last_os_error());
+    }
+    Ok(ptr)
+}
+
+#[cfg(windows)]
+pub(crate) fn free_memory(address: *mut core::ffi::c_void, _size: usize) -> std::io::Result<()> {
+    use windows_sys::Win32::System::Memory::{VirtualFree, MEM_RELEASE};
+
+    let res = unsafe { VirtualFree(address, 0, MEM_RELEASE) };
+
+    if res == 0 {
+        return Err(std::io::Error::last_os_error());
+    }
+
+    Ok(())
+}
+
+#[cfg(unix)]
+pub(crate) fn free_memory(address: *mut core::ffi::c_void, size: usize) -> std::io::Result<()> {
+    use libc::munmap;
+
+    let res = unsafe { munmap(address, size) };
+
+    if res == -1 {
+        panic!("Failed to free memory {}", std::io::Error::last_os_error());
+    }
+
+    Ok(())
+}

67f49035362e806e3262307f5099482fbfeae94b
alborrajo <alvarolopezborr@gmail.com>
2025-12-10 19:51:07 +0100

    Migrate NSAutoreleasePool state to thread local state
    Change-Id: Ie80ce3a9bbd79a70207cd41dc9b3ffb51d47d157

--- src/frameworks/foundation.rs
+++ src/frameworks/foundation.rs
@@ -115,7 +115,6 @@ pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
 
 #[derive(Default)]
 pub struct State {
-    ns_autorelease_pool: ns_autorelease_pool::State,
     ns_bundle: ns_bundle::State,
     ns_file_manager: ns_file_manager::State,
     ns_locale: ns_locale::State,
@@ -129,6 +128,7 @@ pub struct State {
 
 #[derive(Default)]
 pub struct ThreadLocalState {
+    ns_autorelease_pool: ns_autorelease_pool::ThreadLocalState,
     ns_run_loop: ns_run_loop::ThreadLocalState,
 }
 

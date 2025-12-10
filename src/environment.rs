ac50fb13cc9413c4e0830b1f5743d4444ab1b2eb
alborrajo <alvarolopezborr@gmail.com>
2025-12-10 19:45:28 +0100

    Add thread local state
    Change-Id: If44c3e49b785ed97ff25b4e8196cdf5d2cfdf309

--- src/environment.rs
+++ src/environment.rs
@@ -34,6 +34,8 @@ pub struct Thread {
     /// If this is not [ThreadBlock::NotBlocked], the thread is not executing
     /// until a certain condition is fufilled.
     pub blocked_by: ThreadBlock,
+    /// Container for thread local state of various child modules
+    pub thread_local_framework_state: frameworks::ThreadLocalState,
     /// Set to [true] when a thread is running its startup routine (i.e. the
     /// function pointer passed to `pthread_create`). When it returns to the
     /// host, it should become inactive.
@@ -369,6 +371,7 @@ impl Environment {
             in_host_function: false,
             context: None,
             stack: Some(mem::Mem::MAIN_THREAD_STACK_LOW_END..=0u32.wrapping_sub(1)),
+            thread_local_framework_state: Default::default(),
         };
 
         let mut env = Environment {
@@ -547,6 +550,7 @@ impl Environment {
             in_host_function: false,
             context: None,
             stack: Some(mem::Mem::MAIN_THREAD_STACK_LOW_END..=0u32.wrapping_sub(1)),
+            thread_local_framework_state: Default::default(),
         };
 
         let mut env = Environment {
@@ -694,6 +698,7 @@ impl Environment {
             in_host_function: false,
             context: Some(cpu::CpuContext::new()),
             stack: Some(stack_alloc.to_bits()..=(stack_high_addr - 1)),
+            thread_local_framework_state: Default::default(),
         });
         let new_thread_id = self.threads.len() - 1;
 
@@ -713,6 +718,10 @@ impl Environment {
         self.switch_thread(old_thread);
 
         new_thread_id
+    }
+
+    pub fn get_tl_framework_state(&mut self) -> &mut frameworks::ThreadLocalState {
+        &mut self.threads[self.current_thread].thread_local_framework_state
     }
 
     /// Put the current thread to sleep for some duration, running other threads

d9feda47358a31e01830b49b05a8a8cc483da7c9
kouta <darkfm@vera.com.uy>
2026-01-04 19:59:04 -0300

    Implement class_getSuperclass
    Change-Id: I1981ce5ff27afdaf4c29b928b54bc38b411da389

--- src/objc.rs
+++ src/objc.rs
@@ -42,7 +42,10 @@ pub use selectors::{selector, SEL};
 
 use crate::mem::ConstVoidPtr;
 use crate::Environment;
-use classes::{ClassHostObject, FakeClass, UnimplementedClass, objc_getClass, object_getClass};
+use classes::{
+    ClassHostObject, FakeClass, UnimplementedClass, objc_getClass, object_getClass,
+    class_getSuperclass, class_getInstanceMethod, method_getImplementation, method_setImplementation,
+};
 use messages::{
     objc_msgSend, objc_msgSendSuper2, objc_msgSend_stret, MsgSendSignature, MsgSendSuperSignature,
 };
@@ -137,5 +140,9 @@ const FUNCTIONS: FunctionExports = &[
     export_c_func!(sel_registerName(_)),
     export_c_func!(objc_getClass(_)),
     export_c_func!(object_getClass(_)),
+    export_c_func!(class_getSuperclass(_)),
+    export_c_func!(class_getInstanceMethod(_, _)),
+    export_c_func!(method_getImplementation(_)),
+    export_c_func!(method_setImplementation(_, _)),
     export_c_func!(_Block_object_dispose(_, _)),
 ];

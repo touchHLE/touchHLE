962fe7f1dd8ec00984412f01ca41c24f9de0d5cb
kouta <darkfm@vera.com.uy>
2026-01-04 20:53:00 -0300

    Add fake implementation of setApplicationSupportsShakeToEdit.
    Change-Id: I945b956ac674bda3281ad254b382bcaef18a999e

--- src/frameworks/uikit/ui_application.rs
+++ src/frameworks/uikit/ui_application.rs
@@ -191,6 +191,10 @@ pub const CLASSES: ClassExports = objc_classes! {
     log!("TODO: ignoring setApplicationIconBadgeNumber:{}", bn);
 }
 
+- (())setApplicationSupportsShakeToEdit:(bool)enable {
+    log!("TODO: ignoring setApplicationSupportsShakeToEdit:{}",enable);
+}
+
 // UIResponder implementation
 // From the Apple UIView docs regarding [UIResponder nextResponder]:
 // "The shared UIApplication object normally returns nil, but it returns its

fd4a8cd6169448b6ceab142da712070250751746
kouta <darkfm@vera.com.uy>
2026-01-04 21:19:00 -0300

    Add mocked implementation of setImageEdgeInsets
    Change-Id: I66630d0cbaa34c83357a279295cc86ab50f7139b

--- src/frameworks/uikit/ui_view/ui_control/ui_button.rs
+++ src/frameworks/uikit/ui_view/ui_control/ui_button.rs
@@ -391,6 +391,10 @@ pub const CLASSES: ClassExports = objc_classes! {
     }
 }
 
+- (()) setImageEdgeInsets:(CGRect)rect {
+    log!("setImageEdgeInsets: {:?}", rect);
+}
+
 @end
 
 // Undocumented classes used by NIBs

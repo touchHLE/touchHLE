253322d56405cddf2eddcf68e11b4e07851dca81
alborrajo <alvarolopezborr@gmail.com>
2026-01-19 23:38:06 +0100

    Implement task_info with mock values
    Change-Id: Ibb779d12e909212313541d106a1528f9db53049f

--- src/libc/mach/policy.rs
+++ src/libc/mach/policy.rs
@@ -3,7 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+//! Policy
 
-pub mod task;
-pub mod thread_act;
-pub mod vm_types;
+pub type policy_t = i32;

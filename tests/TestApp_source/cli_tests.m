063a17295bcb4564b9eadacc368817836e4ac147
alborrajo <alvarolopezborr@gmail.com>
2026-01-19 23:34:41 +0100

    Handle statically allocated pthread_cond_t
    Change-Id: I58c7a9062bf53f0952e8be7f60ce21bf01017f5d

--- tests/TestApp_source/cli_tests.m
+++ tests/TestApp_source/cli_tests.m
@@ -1138,8 +1138,8 @@ int test_thread_suspend_resume() {
 }
 
 int done = 0, done2 = 0;
-pthread_mutex_t m;
-pthread_cond_t c, c2;
+pthread_mutex_t m, m_static;
+pthread_cond_t c, c2, c_static, c2_static;
 
 void thr_exit() {
   pthread_mutex_lock(&m);
@@ -1190,6 +1190,54 @@ int test_cond_var() {
   done = 1;
   pthread_cond_broadcast(&c);
   pthread_mutex_unlock(&m);
+  pthread_join(p1, NULL);
+  pthread_join(p2, NULL);
+  pthread_join(p3, NULL);
+
+  return done == 1 ? 0 : -1;
+}
+
+void thr_exit_static() {
+  pthread_mutex_lock(&m_static);
+  done = 1;
+  pthread_cond_signal(&c_static);
+  pthread_mutex_unlock(&m_static);
+}
+
+void *child_static(void *arg) {
+  thr_exit_static();
+  return NULL;
+}
+
+void thr_join_static() {
+  pthread_mutex_lock(&m_static);
+  while (done == 0) {
+    pthread_cond_wait(&c_static, &m_static);
+  }
+  pthread_mutex_unlock(&m_static);
+}
+
+int test_cond_var_static() {
+  pthread_t p;
+  done = 0;
+
+  // We test that statically allocated cond vars and mutexes work
+  // by using them without calling pthread_mutex_init and pthread_cont_init
+
+  pthread_create(&p, NULL, child_static, NULL);
+  thr_join_static();
+
+  // Should wake up all threads
+  pthread_t p1, p2, p3;
+  pthread_cond_init(&c2_static, NULL);
+  pthread_create(&p1, NULL, child_static, NULL);
+  pthread_create(&p2, NULL, child_static, NULL);
+  pthread_create(&p3, NULL, child_static, NULL);
+  usleep(100);
+  pthread_mutex_lock(&m_static);
+  done = 1;
+  pthread_cond_broadcast(&c_static);
+  pthread_mutex_unlock(&m_static);
   pthread_join(p1, NULL);
   pthread_join(p2, NULL);
   pthread_join(p3, NULL);
@@ -4309,6 +4357,7 @@ struct {
     FUNC_DEF(test_open),
     FUNC_DEF(test_close),
     FUNC_DEF(test_cond_var),
+    FUNC_DEF(test_cond_var_static),
     FUNC_DEF(test_pthread_mutex_normal),
     FUNC_DEF(test_pthread_mutex_recursive_trylock),
     FUNC_DEF(test_CFMutableDictionary_NullCallbacks),

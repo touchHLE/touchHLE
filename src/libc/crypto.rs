096e0c4e3d18e6cfb82171bcdf0fd84bee074e47
alborrajo <alvarolopezborr@gmail.com>
2026-01-23 18:17:10 +0100

    Implement CC_SHA1 using the sha1 crate
    Change-Id: If29c9e54d941211825f6a0d9b152824282005173

--- src/libc/crypto.rs
+++ src/libc/crypto.rs
@@ -8,6 +8,7 @@
 use crate::dyld::FunctionExports;
 use crate::mem::{ConstVoidPtr, MutPtr};
 use crate::{export_c_func, Environment};
+use sha1::{Digest, Sha1};
 use std::ops::Deref;
 
 fn CC_MD5(env: &mut Environment, data: ConstVoidPtr, len: u32, md: MutPtr<u8>) -> MutPtr<u8> {
@@ -16,4 +17,15 @@ fn CC_MD5(env: &mut Environment, data: ConstVoidPtr, len: u32, md: MutPtr<u8>) -
     md
 }
 
-pub const FUNCTIONS: FunctionExports = &[export_c_func!(CC_MD5(_, _, _))];
+fn CC_SHA1(env: &mut Environment, data: ConstVoidPtr, len: u32, md: MutPtr<u8>) -> MutPtr<u8> {
+    let mut hasher = Sha1::new();
+    hasher.update(env.mem.bytes_at(data.cast(), len));
+    let digest = hasher.finalize();
+    env.mem.bytes_at_mut(md, 20).copy_from_slice(&digest[..]);
+    md
+}
+
+pub const FUNCTIONS: FunctionExports = &[
+    export_c_func!(CC_MD5(_, _, _)),
+    export_c_func!(CC_SHA1(_, _, _)),
+];

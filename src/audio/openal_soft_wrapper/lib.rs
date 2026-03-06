36580c00d4cce2d766112432f5ffb6b2fef218a6
apexad <1437332+apexad@users.noreply.github.com>
2026-03-06 03:17:55 -0700

    Reduce excessive logs and improve OpenAL Errors
    Change-Id: Idfb81746400e7fcae7545db371210ae1e5a93699

--- src/audio/openal_soft_wrapper/lib.rs
+++ src/audio/openal_soft_wrapper/lib.rs
@@ -58,6 +58,14 @@ pub mod alc_defines {
     pub const ALC_SYNC: ALCint = 0x1009;
     pub const ALC_MONO_SOURCES: ALCint = 0x1010;
     pub const ALC_STEREO_SOURCES: ALCint = 0x1011;
+
+    // ALC errors.
+    // https://www.openal.org/documentation/openal-1.1-specification.pdf (Section 6.3.6)
+    pub const ALC_INVALID_DEVICE: ALCenum = 0xA001;
+    pub const ALC_INVALID_CONTEXT: ALCenum = 0xA002;
+    pub const ALC_INVALID_ENUM: ALCenum = 0xA003;
+    pub const ALC_INVALID_VALUE: ALCenum = 0xA004;
+    pub const ALC_OUT_OF_MEMORY: ALCenum = 0xA005;
 }
 
 extern "C" {

494a4a346ea7f776735c5d49add33778f56e92a3
hr56g45678888eh-arch <hr56g45678888eh@gmail.com>
2026-03-24 11:50:55 +0200

    Update sqlite.rs

--- src/libc/sqlite.rs
+++ src/libc/sqlite.rs
@@ -3,31 +3,59 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-//! SQLite3 Stubs
 
-use crate::dyld::export_c_func;
-use crate::mem::{ConstPtr, MutPtr, MutVoidPtr, Ptr};
+use crate::dyld::{export_c_func, FunctionExports};
+use crate::mem::{ConstPtr, MutPtr, SafeRead};
 use crate::Environment;
 
-const SQLITE_OK: i32 = 0;
+// Типы SQLite (просто указатели для гостевой системы)
+pub type Sqlite3Ptr = MutPtr<u32>;
+pub type Sqlite3StmtPtr = MutPtr<u32>;
 
-fn sqlite3_open(
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct Sqlite3;
+unsafe impl SafeRead for Sqlite3 {}
+
+pub fn sqlite3_open(
     env: &mut Environment,
-    _filename: ConstPtr<u8>,
-    pp_db: MutPtr<MutVoidPtr>,
+    filename: ConstPtr<u8>,
+    pp_db: MutPtr<Sqlite3Ptr>,
 ) -> i32 {
-    log!("sqlite3_open called (STUB)");
-    // Записываем фиктивный адрес в указатель
-    env.mem.write(pp_db, Ptr::from_bits(0xBAADF00D));
-    SQLITE_OK
+    let name = env.mem.cstr_at_utf8(filename).unwrap_or("unknown");
+    log!("sqlite3_open({:?})", name);
+    
+    // В реальной имплементации здесь должна быть логика открытия файла.
+    // Пока возвращаем заглушку (SQLITE_OK = 0)
+    0
 }
 
-fn sqlite3_close(_env: &mut Environment, _db: MutVoidPtr) -> i32 {
-    log!("sqlite3_close called (STUB)");
-    SQLITE_OK
+pub fn sqlite3_close(_env: &mut Environment, _db: Sqlite3Ptr) -> i32 {
+    log_dbg!("sqlite3_close");
+    0
 }
 
-pub const FUNCTIONS: crate::dyld::FunctionExports = &[
-    export_c_func!(sqlite3_open(_, _, _)),
-    export_c_func!(sqlite3_close(_, _)),
+pub fn sqlite3_exec(
+    env: &mut Environment,
+    _db: Sqlite3Ptr,
+    sql: ConstPtr<u8>,
+    _callback: u32,
+    _arg: u32,
+    _errmsg: MutPtr<MutPtr<u8>>,
+) -> i32 {
+    let query = env.mem.cstr_at_utf8(sql).unwrap_or("invalid sql");
+    log!("sqlite3_exec: {}", query);
+    0
+}
+
+pub fn sqlite3_finalize(_env: &mut Environment, _stmt: Sqlite3StmtPtr) -> i32 {
+    log_dbg!("sqlite3_finalize");
+    0
+}
+
+pub const FUNCTIONS: FunctionExports = &[
+    export_c_func!(sqlite3_open(_, _)),
+    export_c_func!(sqlite3_close(_)),
+    export_c_func!(sqlite3_exec(_, _, _, _, _)),
+    export_c_func!(sqlite3_finalize(_)),
 ];

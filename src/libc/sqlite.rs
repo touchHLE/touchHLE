1942355e084bf292b2e3180a32142e3bc3f1cdb5
hr56g45678888eh-arch <hr56g45678888eh@gmail.com>
2026-03-24 10:35:21 +0200

    Update sqlite.rs

--- src/libc/sqlite.rs
+++ src/libc/sqlite.rs
@@ -1,22 +1,35 @@
-use crate::Environment;
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+//! SQLite3 Stubs
+
+use crate::dyld::export_c_func;
 use crate::mem::{ConstPtr, MutPtr, MutVoidPtr, Ptr};
-use crate::dyld::FunctionExports;
+use crate::Environment;
 
-// SQLITE_OK = 0
-fn sqlite3_open(env: &mut Environment, _filename: ConstPtr<u8>, pp_db: MutPtr<MutVoidPtr>) -> i32 {
-    log!("STUB: sqlite3_open called");
-    // Записываем "фейковый" указатель на структуру БД, чтобы игра не видела NULL
-    env.mem.write(pp_db, Ptr::from_raw(0xBAADF00D as _)); 
-    0 
+const SQLITE_OK: i32 = 0;
+
+fn sqlite3_open(
+    env: &mut Environment,
+    _filename: ConstPtr<u8>,
+    pp_db: MutPtr<MutVoidPtr>,
+) -> i32 {
+    log!("sqlite3_open called (STUB)");
+
+    // Write a dummy pointer to prevent the app from seeing NULL.
+    env.mem.write(pp_db, Ptr::from_raw(0xBAADF00D as _));
+
+    SQLITE_OK
 }
 
-fn sqlite3_close(_env: &mut Environment, _db: MutVoidPtr) -> i32 {
-    log!("STUB: sqlite3_close called");
-    0
+fn sqlite3_close(_env: &mut Environment, db: MutVoidPtr) -> i32 {
+    log!("sqlite3_close({:?}) called (STUB)", db);
+    SQLITE_OK
 }
 
-pub const FUNCTIONS: FunctionExports = &[
-    export_c_func!(sqlite3_open(_, _)),
+pub const FUNCTIONS: crate::dyld::FunctionExports = &[
+    export_c_func!(sqlite3_open(_, _, _)),
     export_c_func!(sqlite3_close(_)),
 ];
-

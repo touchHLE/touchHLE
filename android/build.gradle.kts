e6e424df201a155eeee6f2538a61d66733fa481e
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2026-04-05 17:24:13 -0600

    Migrate gradle files from groovy to kotlin dsl
    This is a nice to have. While libsdl doesn't so this the compile time
    checking and integration with studio are neat. This is also the
    standard for android build scripts going forward (since AGP 8.4)
    
    Change-Id: I3d1122ef37d87bfdd8436d4c61aa494a33f17554

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,23 +6,18 @@
  * Parts of this file are derived from SDL 2's Android project template, which
  * has a different license. Please see vendor/SDL/LICENSE.txt for details.
  */
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all subprojects/modules.
 
 buildscript {
-    ext {
-        kotlin_version = '2.0.21'
-    }
     repositories {
         mavenCentral()
         google()
-        maven {
-            url "https://plugins.gradle.org/m2/"
-        }
+        gradlePluginPortal()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.10.1'
-        classpath "gradle.plugin.com.github.willir.rust:plugin:0.3.4"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:8.10.1")
+        classpath("gradle.plugin.com.github.willir.rust:plugin:0.3.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -36,6 +31,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean") {
+    delete(layout.buildDirectory)
 }
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -6,4 +6,4 @@
  * Parts of this file are derived from SDL 2's Android project template, which
  * has a different license. Please see vendor/SDL/LICENSE.txt for details.
  */
-include ':app'
+include(":app")

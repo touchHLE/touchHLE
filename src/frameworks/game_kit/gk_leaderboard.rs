378e3b17fd8de0c13f4ba78ff1cdc79dd961d517
zazatree <z88qo@protonmail.com>
2026-05-03 18:13:51 +0400

    Stub for GKLeaderboard class
    Change-Id: Ic5d4f070cd83a32e8be73a5bb8850c9b24304b39

---
+++ src/frameworks/game_kit/gk_leaderboard.rs
@@ -0,0 +1,23 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+//! `GKLeaderboard`.
+
+use crate::objc::{id, nil, objc_classes, ClassExports};
+
+pub const CLASSES: ClassExports = objc_classes! {
+
+(env, this, _cmd);
+
+@implementation GKLeaderboard: NSObject
+
+- (id)init {
+    // TODO
+    nil
+}
+
+@end
+
+};

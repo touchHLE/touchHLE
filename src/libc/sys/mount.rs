0251d6985405537959dc805757fb05dc2793256b
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2026-05-12 12:39:34 -0600

    Stub fstatfs implementation
    Change-Id: Ia4c7339cc38535354b057b2dc88c0001d60f64fa

--- src/libc/sys/mount.rs
+++ src/libc/sys/mount.rs
@@ -6,8 +6,11 @@
 //! `sys/mount.h`, file system statistics
 
 use crate::dyld::{export_c_func, FunctionExports};
+use crate::fs::GuestPath;
 use crate::libc::dirent::MAXPATHLEN;
+use crate::libc::errno::{set_errno, EBADF, ENOENT};
 use crate::libc::posix_io::stat::uid_t;
+use crate::libc::posix_io::{FileDescriptor, STDERR_FILENO, STDIN_FILENO, STDOUT_FILENO};
 use crate::mem::{ConstPtr, MutPtr, SafeRead};
 use crate::Environment;
 
@@ -43,13 +46,7 @@ pub struct statfs {
 }
 unsafe impl SafeRead for statfs {}
 
-pub fn statfs_inner(env: &mut Environment, path: ConstPtr<u8>) -> (i32, statfs) {
-    // FIXME does directory matter?
-    assert!(env
-        .mem
-        .cstr_at_utf8(path)
-        .is_ok_and(|path| path.starts_with(env.fs.home_directory().join("Documents").as_str())));
-
+fn fake_statfs() -> statfs {
     // Values are taken from a test run of iOS 4.3 Simulator
     let mut statfs = statfs {
         f_bsize: 4096,
@@ -74,13 +71,65 @@ pub fn statfs_inner(env: &mut Environment, path: ConstPtr<u8>) -> (i32, statfs) 
     statfs.f_fstypename[..3].copy_from_slice(b"hfs");
     statfs.f_mntonname[..1].copy_from_slice(b"/");
     statfs.f_mntfromname[..12].copy_from_slice(b"/dev/disk0s2");
-    (0, statfs)
+    statfs
+}
+
+/// Internal helper for `statfs`, not a part of the API.
+pub fn statfs_inner(env: &mut Environment, path: ConstPtr<u8>) -> Result<statfs, i32> {
+    // FIXME does directory matter?
+    assert!(env
+        .mem
+        .cstr_at_utf8(path)
+        .is_ok_and(|path| path.starts_with(env.fs.home_directory().join("Documents").as_str())));
+
+    // TODO: Handle additional errors
+    let path = env.mem.cstr_at_utf8(path).unwrap();
+    if !env.fs.exists(GuestPath::new(path)) {
+        return Err(ENOENT);
+    }
+
+    Ok(fake_statfs())
 }
 
 fn statfs(env: &mut Environment, path: ConstPtr<u8>, buf: MutPtr<statfs>) -> i32 {
-    let (ret, statfs) = statfs_inner(env, path);
-    env.mem.write(buf, statfs);
-    ret
+    // TODO: handle errno properly
+    set_errno(env, 0);
+
+    let result = match statfs_inner(env, path) {
+        Ok(statfs) => {
+            env.mem.write(buf, statfs);
+            0
+        }
+        Err(error) => {
+            set_errno(env, error);
+            -1
+        }
+    };
+
+    log!(
+        "TODO: statfs({:?}, {buf:?}) -> {result}",
+        env.mem.cstr_at_utf8(path)
+    );
+    result
 }
 
-pub const FUNCTIONS: FunctionExports = &[export_c_func!(statfs(_, _))];
+fn fstatfs(env: &mut Environment, fd: FileDescriptor, buf: MutPtr<statfs>) -> i32 {
+    // TODO: handle errno properly
+    set_errno(env, 0);
+
+    let result = if !matches!(fd, STDIN_FILENO | STDOUT_FILENO | STDERR_FILENO)
+        && !env.libc_state.posix_io.is_fd_open(fd)
+    {
+        set_errno(env, EBADF);
+        -1
+    } else {
+        env.mem.write(buf, fake_statfs());
+        0
+    };
+
+    log!("TODO: fstatfs({fd}, {buf:?}) -> {result}");
+    result
+}
+
+pub const FUNCTIONS: FunctionExports =
+    &[export_c_func!(statfs(_, _)), export_c_func!(fstatfs(_, _))];

b9750c2d43ea4887a6b53cddc89f1f38e4fd6a12
apexad <1437332+apexad@users.noreply.github.com>
2026-05-18 19:12:02 -0600

    Add UIToolbar and UIBarButtonItem
    Change-Id: I6cc20b9d7053c773b0a0332f006c073346c5f23d

--- src/frameworks/uikit/ui_view/ui_control.rs
+++ src/frameworks/uikit/ui_view/ui_control.rs
@@ -8,6 +8,7 @@
 //! Useful resources:
 //! - The [Target-Action section](https://developer.apple.com/library/archive/documentation/General/Conceptual/CocoaEncyclopedia/Target-Action/Target-Action.html) of Apple's "Concepts in Objective-C Programming".
 
+pub mod ui_bar_button_item;
 pub mod ui_button;
 pub mod ui_segmented_control;
 pub mod ui_slider;

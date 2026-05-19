061486a4e8931bd003ba123cd4bf65bae721c5a7
zazatree <z88qo@protonmail.com>
2026-05-19 23:36:18 +0400

    Implement CMMotionManager isAccelerometerAvailable
    Change-Id: I05ad049986f9e47165e5dc0310c16fa51d9bc58a

--- src/frameworks/core_motion.rs
+++ src/frameworks/core_motion.rs
@@ -33,6 +33,11 @@ const CLASSES: ClassExports = objc_classes! {
     // method. (All devices have accelerometer, but only some do have gyro).
     false
 }
+- (bool)isAccelerometerAvailable {
+    // According to https://developer.apple.com/documentation/coremotion/getting-raw-accelerometer-events?language=objc
+    // every iOS device has an accelerometer.
+    true
+}
 
 @end
 

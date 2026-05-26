7577e89bddbaec97ef99bcd44541e683c9af9125
Alex <Alex9303Github@protonmail.com>
2026-05-26 14:45:14 -0400

    Implement stubs for UIBarButtonItem
    Change-Id: Icb59789194fe107a3452dd0779a18ac34a5eb7f2

--- src/frameworks/uikit.rs
+++ src/frameworks/uikit.rs
@@ -15,6 +15,7 @@ use std::time::Instant;
 pub mod ui_accelerometer;
 pub mod ui_activity_indicator_view;
 pub mod ui_application;
+pub mod ui_bar_button_item;
 pub mod ui_color;
 pub mod ui_device;
 pub mod ui_event;
@@ -39,6 +40,7 @@ pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
         ui_accelerometer::CLASSES,
         ui_activity_indicator_view::CLASSES,
         ui_application::CLASSES,
+        ui_bar_button_item::CLASSES,
         ui_color::CLASSES,
         ui_device::CLASSES,
         ui_event::CLASSES,

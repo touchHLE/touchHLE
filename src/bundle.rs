29b0444c00688c32d79703a5d31060e70db60641
apexad <1437332+apexad@users.noreply.github.com>
2026-05-27 09:59:34 -0600

    Fix icon_path to use CFBundleIconFile/CFBundleIconFiles/Icon.png
    Change-Id: Ie0ff6c73742845e749759b6c062963f3686fa2a4

--- src/bundle.rs
+++ src/bundle.rs
@@ -156,7 +156,29 @@ impl Bundle {
     }
 
     fn icon_path(&self) -> GuestPathBuf {
-        if let Some(filename) = self.plist.get("CFBundleIconFile") {
+        // TODO: Fix this to what an actual iOS device does,
+        // including Retina icons and such when we get there.
+        // Reference: https://developer.apple.com/library/archive/qa/qa1686/_index.html
+        // We check for the icon in the following order:
+        // CFBundleIconFile (adds .png if not present),
+        // Using CFBundleIconsFiles Icon.png, Icon-72.png,
+        // First in CFBundleIconFiles (adds .png if not present),
+        // Failsafe Icon.png
+        if let Some(filename) = self.plist.get("CFBundleIconFile").or_else(|| {
+            self.plist
+                .get("CFBundleIconFiles")
+                .and_then(|v| v.as_array())
+                .and_then(|icon_files| {
+                    ["Icon.png", "Icon-72.png"]
+                        .iter()
+                        .find_map(|icon| {
+                            icon_files.iter().find(|found_icon| {
+                                found_icon.as_string().is_some_and(|s| s == *icon)
+                            })
+                        })
+                        .or_else(|| icon_files.first())
+                })
+        }) {
             if filename
                 .as_string()
                 .unwrap()

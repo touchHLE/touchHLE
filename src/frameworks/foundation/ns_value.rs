7e6b2b7f3c65b0557102f20c3dba0fc6619066ee
abnormalmaps <abnormalmaps@gmail.com>
2026-05-26 20:19:18 -0400

    Add NSValue number/initWithUnsignedInteger
    Change-Id: I6d2110f0fa88b861371ea6c0ece87f931818f04e

--- src/frameworks/foundation/ns_value.rs
+++ src/frameworks/foundation/ns_value.rs
@@ -235,6 +235,14 @@ pub const CLASSES: ClassExports = objc_classes! {
     autorelease(env, new)
 }
 
++ (id)numberWithUnsignedInteger:(NSUInteger)value {
+    // TODO: for greater efficiency we could return a static-lifetime value
+
+    let new: id = msg![env; this alloc];
+    let new: id = msg![env; new initWithUnsignedInteger:value];
+    autorelease(env, new)
+}
+
 + (id)numberWithLongLong:(i64)value {
     // TODO: for greater efficiency we could return a static-lifetime value
 
@@ -341,6 +349,11 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 - (id)initWithInteger:(NSInteger)value {
     *env.objc.borrow_mut(this) = NSNumberHostObject::Int(value);
+    this
+}
+
+- (id)initWithUnsignedInteger:(NSUInteger)value {
+    *env.objc.borrow_mut(this) = NSNumberHostObject::UnsignedInt(value);
     this
 }
 

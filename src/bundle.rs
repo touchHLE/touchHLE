e7abdf61ef4244425bc5473ba06739b515eab2a8
apexad <1437332+apexad@users.noreply.github.com>
2026-05-27 09:59:34 -0600

    check CFBundleIconFiles for largest icon, then use CFBundleIconFile
    Change-Id: Ie0ff6c73742845e749759b6c062963f3686fa2a4

--- src/bundle.rs
+++ src/bundle.rs
@@ -156,7 +156,28 @@ impl Bundle {
     }
 
     fn icon_path(&self) -> GuestPathBuf {
-        if let Some(filename) = self.plist.get("CFBundleIconFile") {
+        if let Some(filename) = self
+            .plist
+            .get("CFBundleIconFiles")
+            .and_then(|v| v.as_array())
+            .and_then(|a| {
+                // List created using icon sizes listed here
+                // https://developer.apple.com/library/archive/qa/qa1686/_index.html
+                [
+                    "60@3x", "83.5@2x", "76@2x", "72@2x", "60@2x", "@2x", "76", "72", "60",
+                ]
+                .iter()
+                .find_map(|pref| {
+                    a.iter().find(|v| {
+                        v.as_string().is_some_and(|s| {
+                            s.to_lowercase().trim_end_matches(".png").ends_with(*pref)
+                        })
+                    })
+                })
+                .or_else(|| a.first())
+            })
+            .or_else(|| self.plist.get("CFBundleIconFile"))
+        {
             if filename
                 .as_string()
                 .unwrap()

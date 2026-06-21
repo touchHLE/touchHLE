7400bdc1fb5551d8ce4ffd9d75a0cd96f5ca9151
zazatree <z88qo@protonmail.com>
2026-06-21 17:38:27 +0400

    Implement CALayer contentsScale and setContentsScale:
    Change-Id: Ib70d3c930005098f67c4e88af01118a06cbdc7a9

--- src/frameworks/core_animation/ca_layer.rs
+++ src/frameworks/core_animation/ca_layer.rs
@@ -48,6 +48,7 @@ pub(super) struct CALayerHostObject {
     pub(super) opacity: f32,
     pub(super) background_color: Option<CGColorHostObject>,
     pub(super) corner_radius: CGFloat,
+    pub(super) contents_scale: CGFloat,
     pub(super) needs_display: bool,
     pub(super) needs_display_on_bounds_change: bool,
     /// `CGImageRef*`
@@ -126,6 +127,7 @@ pub const CLASSES: ClassExports = objc_classes! {
         opacity: 1.0,
         background_color: None, // transparency
         corner_radius: 0.0,
+        contents_scale: 1.0,
         needs_display: false,
         needs_display_on_bounds_change: false,
         contents: nil,
@@ -348,6 +350,13 @@ pub const CLASSES: ClassExports = objc_classes! {
 }
 - (())setCornerRadius:(CGFloat)corner_radius {
     env.objc.borrow_mut::<CALayerHostObject>(this).corner_radius = corner_radius;
+}
+
+- (CGFloat)contentsScale {
+    env.objc.borrow::<CALayerHostObject>(this).contents_scale
+}
+- (())setContentsScale:(CGFloat)contents_scale {
+    env.objc.borrow_mut::<CALayerHostObject>(this).contents_scale = contents_scale;
 }
 
 - (bool)needsDisplay {

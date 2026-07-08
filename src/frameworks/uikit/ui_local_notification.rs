89bfdad0160254a8f269efea5f1d14797374cf32
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2026-07-08 00:09:33 -0400

    Add setApplicationIconBadgeNumber stub
    Change-Id: If39751d2aaafbffcbdcce750fa83f8bf312b52b6

--- src/frameworks/uikit/ui_local_notification.rs
+++ src/frameworks/uikit/ui_local_notification.rs
@@ -6,6 +6,7 @@
 //! `UILocalNotification`.
 
 use crate::objc::{id, objc_classes, todo_objc_setter, ClassExports};
+use crate::frameworks::foundation::NSInteger;
 
 pub const CLASSES: ClassExports = objc_classes! {
 
@@ -27,6 +28,9 @@ pub const CLASSES: ClassExports = objc_classes! {
 }
 - (())setSoundName:(id)name { // NSString *
     todo_objc_setter!(this, name);
+}
+- (())setApplicationIconBadgeNumber:(NSInteger)badge_number {
+    todo_objc_setter!(this, badge_number);
 }
 
 @end

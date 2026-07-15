55fcc219c15ff6937edb2f28956a3e79f1e5fe0d
alborrajo <alvarolopezborr@gmail.com>
2026-07-15 17:27:41 +0200

    UIPageControl stub
    Change-Id: I7a3a6b7fc260f5d5da84b8d7873319338eb1e496

--- src/frameworks/uikit.rs
+++ src/frameworks/uikit.rs
@@ -61,6 +61,7 @@ pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
         ui_view::ui_control::ui_switch::CLASSES,
         ui_view::ui_image_view::CLASSES,
         ui_view::ui_label::CLASSES,
+        ui_view::ui_page_control::CLASSES,
         ui_view::ui_picker_view::CLASSES,
         ui_view::ui_scroll_view::CLASSES,
         ui_view::ui_scroll_view::ui_text_view::CLASSES,

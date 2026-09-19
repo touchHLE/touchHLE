fe39a76eb252752e5ce497d0cc72f5ea380c8ab5
ciciplusplus <ciciplusplus@gmail.com>
2026-09-19 12:34:42 +0200

    Implement _NSGetExecutablePath()
    Change-Id: Ic26f16ce0b7e3c10311f735af8c8ee81152bc7d6

--- src/libc/mach_o.rs
+++ src/libc/mach_o.rs
@@ -6,7 +6,23 @@
 //! `Mach-O` related functions.
 
 use crate::dyld::{export_c_func, FunctionExports};
+use crate::mem::{GuestUSize, MutPtr};
 use crate::Environment;
+
+fn _NSGetExecutablePath(env: &mut Environment, buf: MutPtr<u8>, buf_size: MutPtr<u32>) -> i32 {
+    let binding = env.bundle.executable_path();
+    let bin_path = binding.as_str();
+
+    let size = env.mem.read(buf_size);
+    let len: GuestUSize = bin_path.len().try_into().unwrap();
+    assert!(len < size);
+
+    env.mem
+        .bytes_at_mut(buf, len)
+        .copy_from_slice(bin_path.as_bytes());
+    env.mem.write(buf + len, b'\0');
+    0
+}
 
 fn get_end(env: &mut Environment) -> u32 {
     // Assume app binary is the first.
@@ -34,4 +50,8 @@ fn get_etext(env: &mut Environment) -> u32 {
     text_section.next_section_addr()
 }
 
-pub const FUNCTIONS: FunctionExports = &[export_c_func!(get_end()), export_c_func!(get_etext())];
+pub const FUNCTIONS: FunctionExports = &[
+    export_c_func!(_NSGetExecutablePath(_, _)),
+    export_c_func!(get_end()),
+    export_c_func!(get_etext()),
+];
